b2a9ad52dfe0aa0888b3542098a62de36149d1a6
John Haddon <john@image-engine.com>
2007-11-21 20:29:56 +0000

    Fixed attempted importing of now removed files.

--- python/IECore/__init__.py
+++ python/IECore/__init__.py
@@ -79,11 +79,9 @@ from CheckFileDependenciesOp import CheckFileDependenciesOp
 from PointsExpressionOp import PointsExpressionOp
 from Struct import Struct
 from LsHeaderOp import LsHeaderOp
-from ContextualClassLoader import ContextualClassLoader
 from curry import curry
 from MenuItemDefinition import MenuItemDefinition
 from MenuDefinition import MenuDefinition
-from ApplicationOpLoader import ApplicationOpLoader
 
 # importing internal utility modules and class overwrites
 from ObjectOverwriting import *
