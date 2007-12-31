769995bf1709181a1207e0b28fde4d30145f0640
Mark Williams <mark@image-engine.com>
2007-12-31 23:10:01 +0000

    Removed stray space

--- src/IECore/JPEGImageReader.cpp
+++ src/IECore/JPEGImageReader.cpp
@@ -14,7 +14,7 @@
 //       documentation and/or other materials provided with the distribution.
 //
 //     * Neither the name of Image Engine Design nor the names of any
-//        other contributors to this software may be used to endorse or
+//       other contributors to this software may be used to endorse or
 //       promote products derived from this software without specific prior
 //       written permission.
 //
