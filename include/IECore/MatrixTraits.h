74b28b9adca0da58159fa1e0f69248a554dac344
Mark Williams <mark@image-engine.com>
2008-01-07 19:04:14 +0000

    Removed unnecessary include

--- include/IECore/MatrixTraits.h
+++ include/IECore/MatrixTraits.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -34,6 +34,8 @@
 
 #ifndef IE_CORE_MATRIXTRAITS_H
 #define IE_CORE_MATRIXTRAITS_H
+
+#include <cassert>
 
 #include "OpenEXR/ImathMatrix.h"
 
