c9fb2c4ff1c5e9ec5b44b1c4c6b30b47e9566dc3
Mark Williams <mark@image-engine.com>
2008-01-07 20:43:43 +0000

    Added NotImplementedException

--- include/IECore/Exception.h
+++ include/IECore/Exception.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -110,6 +110,16 @@ namespace IECore
 			PermissionDeniedIOException(const std::string &what) : IOException(what) {};
 			
 			virtual const char *type() const throw() { return "Permission Denied"; }			
+	};
+	
+	/// Base class for Invalid Argument exceptions
+	class NotImplementedException : public Exception
+	{
+		public:
+			NotImplementedException(const char *what) : Exception(what) {};
+			NotImplementedException(const std::string &what) : Exception(what) {};
+			
+			virtual const char *type() const throw() { return "Not Implemented"; }
 	};
 }
 
