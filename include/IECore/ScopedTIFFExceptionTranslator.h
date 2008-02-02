332a50b65b754c695b587b3a7b54870b502d7910
Mark Williams <mark@image-engine.com>
2008-02-02 01:57:32 +0000

    Added class for translating errors generated from libtiff to IECore::IOExceptions

---
+++ include/IECore/ScopedTIFFExceptionTranslator.h
@@ -0,0 +1,63 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Image Engine Design nor the names of any
+//       other contributors to this software may be used to endorse or
+//       promote products derived from this software without specific prior
+//       written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef IE_CORE_SCOPEDTIFFEXCEPTIONTRANSLATOR_H
+#define IE_CORE_SCOPEDTIFFEXCEPTIONTRANSLATOR_H
+
+#include <stdarg.h>
+
+#include "tiffio.h"
+
+namespace IECore
+{
+
+/// A class which can temporarily translate errors from libtiff into IECore::IOExceptions whilst it remains in scope -
+/// it registers a new TIFFErrorHandler in its constructor, restoring the previous state in its destructor.
+class ScopedTIFFExceptionTranslator 
+{
+	public:
+		ScopedTIFFExceptionTranslator( );
+		virtual ~ScopedTIFFExceptionTranslator();
+		
+	protected:
+				
+		static void output(const char* module, const char* fmt, va_list ap);
+		
+		TIFFErrorHandler m_previousHandler;
+			 
+};
+
+} // namespace IECore
+
+#endif // IE_CORE_SCOPEDTIFFEXCEPTIONTRANSLATOR_H
