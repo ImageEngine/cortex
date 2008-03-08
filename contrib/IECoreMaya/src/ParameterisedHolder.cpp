1131061a57c48344ffa4ebab51b0c787c8af8fcb
Mark Williams <mark@image-engine.com>
2008-03-08 01:52:18 +0000

    Improved exception handling

--- contrib/IECoreMaya/src/ParameterisedHolder.cpp
+++ contrib/IECoreMaya/src/ParameterisedHolder.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -353,6 +353,11 @@ MStatus ParameterisedHolder<B>::setNodeValues()
 				return s;
 			}
 		}
+		catch( std::exception &e )
+		{
+			msg( Msg::Error, "ParameterisedHolder::setNodeValues", boost::format( "Caught exception while setting value : %s" ) % e.what());
+			return MStatus::kFailure;
+		}
 		catch( ... )
 		{
 			msg( Msg::Error, "ParameterisedHolder::setNodeValues", "Caught exception while setting value." );
@@ -385,6 +390,11 @@ MStatus ParameterisedHolder<B>::setParameterisedValues()
 			{
 				return s;
 			}
+		}
+		catch( std::exception &e )
+		{
+			msg( Msg::Error, "ParameterisedHolder::setParameterisedValues", boost::format( "Caught exception while setting value : %s" ) % e.what());
+			return MStatus::kFailure;
 		}
 		catch( ... )
 		{
