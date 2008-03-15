6bc7afbd7236648fae252e07c4c3a2ad15c20f49
Lucio Moser <lucio@image-engine.com>
2008-03-15 00:14:21 +0000

    Taking some inexistent tests out of All.py

--- test/IECore/All.py
+++ test/IECore/All.py
@@ -133,11 +133,9 @@ from Math import *
 
 if IECore.withTIFF() :
 	from TIFFReader import *
-	from TIFFImageWriter import *
 
 if IECore.withJPEG() :
 	from JPEGReader import *
-	from JPEGImageWriter import *
 
 if IECore.withSQLite() :
 	from SQLiteIndexedIO import *
