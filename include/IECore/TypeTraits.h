403af675b9bb1d036f2a451dbe7a8bcac87b4c80
Mark Williams <mark@image-engine.com>
2008-03-26 22:43:56 +0000

    Added TypeTraits, to allow some useful properties of basic types to be used in template metaprogramming

---
+++ include/IECore/TypeTraits.h
@@ -0,0 +1,150 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Image Engine Design nor the names of any
+//       other contributors to this software may be used to endorse or
+//       promote products derived from this software without specific prior
+//       written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef IE_CORE_TYPETRAITS_H
+#define IE_CORE_TYPETRAITS_H
+
+#include "boost/static_assert.hpp"
+#include "boost/mpl/or.hpp"
+#include "boost/mpl/and.hpp"
+#include "boost/mpl/if.hpp"
+#include "boost/mpl/not.hpp"
+
+#include "IECore/SimpleTypedData.h"
+#include "IECore/VectorTypedData.h"
+
+#include "OpenEXR/ImathMatrix.h"
+
+namespace IECore
+{
+
+namespace TypeTraits
+{
+
+/// HasValueType
+template<typename T> struct HasValueType : public boost::false_type {};
+template<typename T> struct HasValueType< TypedData<T> > : public boost::true_type {};
+
+namespace Detail
+{
+
+template <class T>
+struct GetValueType
+{
+	typedef typename T::ValueType type;
+};
+
+template <class T>
+struct ValueType
+{
+	typedef typename boost::mpl::eval_if< HasValueType<T>, GetValueType<T>, boost::mpl::identity<T> >::type type;
+};
+
+BOOST_STATIC_ASSERT( (sizeof( ValueType< M33fData >::type ) == sizeof( M33fData::ValueType ) ) );
+BOOST_STATIC_ASSERT( (sizeof( ValueType< FloatVectorData >::type ) == sizeof( FloatVectorData::ValueType ) ) );
+
+} // namespace Detail
+
+/// IsTypedData
+template<typename T> struct IsTypedData : public boost::false_type {};
+template<typename T> struct IsTypedData< TypedData<T> > : public boost::true_type {};
+template<typename T> struct IsTypedData< const TypedData<T> > : public boost::true_type {};
+
+/// IsVectorTypedData
+template<typename T> struct IsVectorTypedData : public boost::false_type {};
+template<typename T> struct IsVectorTypedData< TypedData<std::vector<T> > > : public boost::true_type {};
+template<typename T> struct IsVectorTypedData< const TypedData<std::vector<T> > > : public boost::true_type {};
+
+/// IsSimpleTypedData
+template<typename T> struct IsSimpleTypedData : public boost::false_type {};
+template<typename T> struct IsSimpleTypedData< TypedData<T > > : public boost::true_type {};
+template<typename T> struct IsSimpleTypedData< TypedData<std::vector<T> > > : public boost::false_type {};
+template<typename T> struct IsSimpleTypedData< const TypedData<std::vector<T> > > : public boost::false_type {};
+
+/// IsMatrix33
+template<typename T> struct IsMatrix33 : public boost::false_type {};
+template<typename T> struct IsMatrix33< Imath::Matrix33<T> > : public boost::true_type {};
+
+/// IsMatrix44
+template<typename T> struct IsMatrix44 : public boost::false_type {};
+template<typename T> struct IsMatrix44< Imath::Matrix44<T> > : public boost::true_type {};
+
+/// IsMatrix
+template<typename T> struct IsMatrix : boost::mpl::or_< IsMatrix33<T>, IsMatrix44<T> > {};
+BOOST_STATIC_ASSERT( (IsMatrix< Imath::M33f >::value) );
+BOOST_STATIC_ASSERT( (IsMatrix< Imath::M44d >::value) );
+BOOST_STATIC_ASSERT( (IsMatrix< Detail::GetValueType< M33fData >::type >::value ) );
+BOOST_STATIC_ASSERT( (IsMatrix< Detail::ValueType< M33fData >::type >::value ) );
+BOOST_STATIC_ASSERT( (boost::mpl::not_< IsMatrix< Imath::V2i > >::value) );
+
+/// IsVec3
+template<typename T> struct IsVec3 : public boost::false_type {};
+template<typename T> struct IsVec3< Imath::Vec3<T> > : public boost::true_type {};
+BOOST_STATIC_ASSERT( ( boost::mpl::not_< IsVec3<Imath::V2d> >::value) );
+
+/// IsVec2
+template<typename T> struct IsVec2 : public boost::false_type {};
+template<typename T> struct IsVec2< Imath::Vec2<T> > : public boost::true_type {};
+BOOST_STATIC_ASSERT( ( boost::mpl::not_< IsVec2<Imath::V3i> >::value) );
+
+/// IsVec
+template<typename T> struct IsVec : boost::mpl::or_< IsVec3<T>, IsVec2<T> > {};
+BOOST_STATIC_ASSERT( (IsVec<Imath::V3i>::value) );
+BOOST_STATIC_ASSERT( (IsVec<Imath::V2d>::value) );
+BOOST_STATIC_ASSERT( ( boost::mpl::not_< IsVec<int> >::value) );
+
+/// IsMatrixTypedData
+template< typename T > struct IsMatrixTypedData : boost::mpl::and_< IsTypedData<T>, IsMatrix< typename Detail::ValueType<T>::type > > {};
+BOOST_STATIC_ASSERT( (IsMatrixTypedData<M33fData>::value) );
+BOOST_STATIC_ASSERT( ( boost::mpl::not_< IsMatrixTypedData<V3fData> >::value) );
+BOOST_STATIC_ASSERT( ( boost::mpl::not_< IsMatrixTypedData<char> >::value) );
+
+/// IsVec2TypedData
+template< typename T > struct IsVec2TypedData : boost::mpl::and_< IsTypedData<T>, IsVec2< typename Detail::ValueType<T>::type > > {};
+BOOST_STATIC_ASSERT( (IsVec2TypedData<V2fData>::value) );
+
+/// IsVec3TypedData
+template< typename T > struct IsVec3TypedData : boost::mpl::and_< IsTypedData<T>, IsVec3< typename Detail::ValueType<T>::type > > {};
+BOOST_STATIC_ASSERT( (IsVec3TypedData<V3fData>::value) );
+
+/// IsVecTypedData
+template< typename T > struct IsVecTypedData : boost::mpl::or_< IsVec2TypedData<T>, IsVec3TypedData<T> > {};
+BOOST_STATIC_ASSERT( (IsVecTypedData<V2iData>::value) );
+BOOST_STATIC_ASSERT( (IsVecTypedData<V3fData>::value) );
+
+} // namespace TypeTraits
+
+} // namespace IECore
+
+#endif // IE_CORE_TYPETRAITS_H
