aeef0d6fe62384d62f4fd43269d0112622bcc4b7
Mark Williams <mark@image-engine.com>
2008-04-10 21:19:07 +0000

    Made two members const

--- include/IECore/KDTree.h
+++ include/IECore/KDTree.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -103,8 +103,8 @@ class KDTree
 		
 		Permutation m_perm;
 		NodeVector m_nodes;
-		int m_maxLeafSize; /// \todo make const
-		PointIterator m_lastPoint; /// \todo make const
+		const int m_maxLeafSize;
+		const PointIterator m_lastPoint;
 	
 };
 
