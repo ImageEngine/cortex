933e354420534113b92b1c21403e34c420ffa729
John Haddon <john@image-engine.com>
2008-04-11 03:25:14 +0000

    Added a todo.

--- contrib/IECoreMaya/src/CacheSet.cpp
+++ contrib/IECoreMaya/src/CacheSet.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -39,12 +39,11 @@
 #include "maya/MTime.h"
 #include "maya/MGlobal.h"
 
-#include "typeIds/TypeIds.h"
-
 #include "IECore/Exception.h"
 #include "IECore/OversamplesCalculator.h"
 #include "IECoreMaya/CacheSet.h"
 #include "IECoreMaya/MayaTime.h"
+#include "IECoreMaya/MayaTypeIds.h"
 
 using namespace IECore;
 using namespace IECoreMaya;
