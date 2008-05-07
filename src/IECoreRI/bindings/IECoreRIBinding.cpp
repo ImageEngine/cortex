809ef624faf182d447fe011a2817b666491e23ca
John Haddon <john@image-engine.com>
2008-05-07 01:23:27 +0000

    Removed function which is pointless now IECoreRI version is synched with IECore version.

--- src/IECoreRI/bindings/IECoreRIBinding.cpp
+++ src/IECoreRI/bindings/IECoreRIBinding.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -34,8 +34,6 @@
 
 #include <boost/python.hpp>
 
-#include "IECoreRI/IECoreRI.h"
-
 #include "IECoreRI/bindings/RendererBinding.h"
 #include "IECoreRI/bindings/SLOReaderBinding.h"
 
@@ -53,5 +51,4 @@ BOOST_PYTHON_MODULE( _IECoreRI )
 	bindPTCParticleReader();
 	bindPTCParticleWriter();
 #endif // IECORERI_WITH_PTC
-	def( "coreMajorVersion", &coreMajorVersion );
 }
