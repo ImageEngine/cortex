91f42aa410cfdcb71cf9d8333d9172dfecfc10bb
John Haddon <john@image-engine.com>
2008-06-13 00:04:46 +0000

    Added a todo.

--- contrib/IECoreGL/include/IECoreGL/PointsPrimitive.h
+++ contrib/IECoreGL/include/IECoreGL/PointsPrimitive.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -42,6 +42,7 @@
 namespace IECoreGL
 {
 
+/// \todo Camera facing seems to fail in orthographic mode.
 class PointsPrimitive : public Primitive
 {
 	public :
