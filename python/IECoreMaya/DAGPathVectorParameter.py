24b5825946510f4264a24d934dad259b7f76c1c6
Mark Williams <mark@image-engine.com>
2008-08-07 22:26:08 +0000

    Fixed duplicate type id

--- python/IECoreMaya/DAGPathVectorParameter.py
+++ python/IECoreMaya/DAGPathVectorParameter.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -176,4 +176,4 @@ class DAGPathVectorParameter( IECore.StringVectorParameter ):
 
 		return result
 
-IECore.makeRunTimeTyped( DAGPathVectorParameter, 350002, IECore.StringVectorParameter )
+IECore.makeRunTimeTyped( DAGPathVectorParameter, 350003, IECore.StringVectorParameter )
