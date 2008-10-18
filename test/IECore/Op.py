ecdb7fbb576fd1ae1d17512f1b545d6b401de959
Mark Williams <mark@image-engine.com>
2008-10-18 02:18:08 +0000

    Added test to ensure that op result is set back to resultParameter

--- test/IECore/Op.py
+++ test/IECore/Op.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -61,6 +61,10 @@ class TestPythonOp( unittest.TestCase ) :
 		o = PythonOp()
 		self.assertEqual( o( name = "jim" ), StringData( "jim" ) )
 		self.assertEqual( o( name = StringData( "jimbo" ) ), StringData( "jimbo" ) )
+		
+		result = o( name = "roger" )
+		self.assertEqual( result, StringData( "roger" ) )
+		self.assertEqual( o.resultParameter().getValue(), result )
 
 	def testDefaultConstructor( self ):
 		import IECore
