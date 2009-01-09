8303fddfa920993e5486246cb8659f620733450d
Mark Williams <mark@image-engine.com>
2009-01-09 01:01:47 +0000

    Made error more descriptive

--- src/IECoreMaya/bindings/FnParameterisedHolderBinding.cpp
+++ src/IECoreMaya/bindings/FnParameterisedHolderBinding.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2008-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -64,7 +64,7 @@ static ParameterisedHolderInterface *interface( MFnDependencyNode *fnDN )
 		}
 	}
 	// failed
-	throw Exception( "Not a ParameterisedHolder" );
+	throw Exception( ( MString("Node \"") + fnDN->name() + "\" is not a ParameterisedHolder" ).asChar() );
 }
 
 static void setParameterised( MFnDependencyNode *fnDN, ParameterisedPtr p )
