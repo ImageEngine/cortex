58c67e37a5c92c30a237fe6fc8d376fa5a3d1c71
Mark Williams <mark@image-engine.com>
2009-01-13 23:15:42 +0000

    Claimed type ids

--- include/IECoreMaya/TypeIds.h
+++ include/IECoreMaya/TypeIds.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2008-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -62,7 +62,9 @@ enum TypeId
 	ToMayaArrayDataConverterTypeId = 109018,
 	ToMayaPlugConverterTypeId = 109019,
 	FromMayaPluginDataPlugConverterTypeId = 109020,	
-	FromMayaTransformConverterTypeId = 109021,	
+	FromMayaTransformConverterTypeId = 109021,
+	FromMayaImageConverterTypeId = 109003,	
+	ToMayaImageConverterTypeId = 109004,	
 	LastTypeId = 109999
 
 };
