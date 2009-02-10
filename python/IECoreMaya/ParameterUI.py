4a6a582e2e1e5f5e9bbd76838ced7261258439e3
John Haddon <john@image-engine.com>
2009-02-10 19:49:49 +0000

    Removing blank lines so I can see a bit more of this function which doesn't fit on one screen.

--- python/IECoreMaya/ParameterUI.py
+++ python/IECoreMaya/ParameterUI.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -174,7 +174,7 @@ class ParameterUI :
 			
 			cmds.setParent("..")
 	
-			return True	
+			return True
 			
 		else:
 		
@@ -273,7 +273,6 @@ class ParameterUI :
 					divider = True
 				)	
 		
-		
 			cmds.menuItem(
 					parent = popupMenu,
 					label = "Default",
@@ -284,7 +283,6 @@ class ParameterUI :
 				parent = popupMenu,
 				divider = True
 			)		
-		
 		
 			controlType = cmds.objectTypeUI( ownerControl)
 			
@@ -337,7 +335,6 @@ class ParameterUI :
 			else:
 			
 				hasConnections = self.__buildConnectionsPopupMenu( popupMenu, ownerControl, **kw )
-					
 		
 			cmds.menuItem(
 					parent = popupMenu,
