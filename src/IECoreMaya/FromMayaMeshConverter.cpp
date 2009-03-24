b896e2b008c8a2fc2eb17603a8c007064b19e7d6
Mark Williams <mark@image-engine.com>
2009-03-24 20:31:09 +0000

    Removed redundant include

--- src/IECoreMaya/FromMayaMeshConverter.cpp
+++ src/IECoreMaya/FromMayaMeshConverter.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -43,7 +43,6 @@
 #include "IECore/NumericParameter.h"
 #include "IECore/MessageHandler.h"
 #include "IECore/DespatchTypedData.h"
-#include "IECore/ClassData.h"
 
 #include "maya/MFn.h"
 #include "maya/MFnMesh.h"
