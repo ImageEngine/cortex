73572802a34fa2f62a86e309bb74f80903a94239
John Haddon <john@image-engine.com>
2009-03-25 00:51:19 +0000

    Added a todo.

--- python/IECore/ClassLoader.py
+++ python/IECore/ClassLoader.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -48,6 +48,8 @@ from IECore import Msg, msg, SearchPath
 # extension classes, and allows us to create small versioned units
 # of functionality for use all over the place - the ieCore "do" script
 # uses the ClassLoader to find operations it can perform for instance.
+## \todo Reduce the overhead of refresh() - perhaps just use it to
+# get the class names, and then get the versions lazily? 
 class ClassLoader :
 
 	## Creates a ClassLoader which will load
