2382ec4ac8093e293eac3c55b7196537718e3102
Mark Williams <mark@image-engine.com>
2009-03-25 21:07:00 +0000

    Tidying up

--- include/IECore/ImageWriter.h
+++ include/IECore/ImageWriter.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -46,7 +46,7 @@ namespace IECore
 
 IE_CORE_FORWARDDECLARE( ImagePrimitive );
 
-/// abstract base class for serializing images
+/// Abstract base class for serializing images
 class ImageWriter : public Writer
 {
 
@@ -54,31 +54,28 @@ class ImageWriter : public Writer
 
 		IE_CORE_DECLARERUNTIMETYPED( ImageWriter, Writer )
 
-		/// checks that object is an ImagePrimitive instance
+		/// Checks that object is an ImagePrimitive instance
 		static bool canWrite( ConstObjectPtr object, const std::string &fileName );
 
-		/// convenience function to access the channels specified in parameters
+		/// Convenience function to access the channels specified in parameters
 		void imageChannels( std::vector<std::string> &names ) const;
 
 	protected:
 	
 		ImageWriter( const std::string &name, const std::string &description );	
 
-		/// Writes the associated ImagePrimitive. There's no need to reimplement
-		/// this in subclasses - implement writeImage() instead.
-		virtual void doWrite();
-
-		/// return the image object to write
+		/// Return the image object to write
 		ConstImagePrimitivePtr getImage() const;
 
-		/// write the image. subclasses implement this method.
+		/// Write the image. Subclasses implement this method.
 		virtual void writeImage( const std::vector<std::string> &names,
 		                         ConstImagePrimitivePtr image,
 		                         const Imath::Box2i &dataWindow	) const = 0;
 
 	private :
-
-		void constructParameters();
+		
+		/// Implementation of Writer::doWrite(). Calls through to writeImage()
+		virtual void doWrite();
 
 		StringVectorParameterPtr m_channelsParameter;
 
