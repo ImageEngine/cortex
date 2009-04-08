d9ac8104c39c377959e3affbb8f11dc9a1899946
Mark Williams <mark@image-engine.com>
2009-04-08 02:02:59 +0000

    Added test

--- test/IECore/LineSegmentTest.py
+++ test/IECore/LineSegmentTest.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2008-2009, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -185,6 +185,17 @@ class LineSegmentTest( unittest.TestCase ) :
 		self.assertEqual( l1.p0, l2.p0 + V3f( 1 ) )
 		self.assertEqual( l1.p1, l2.p1 + V3f( 1 ) )
 		
+	def testIntersect( self ) :
+	
+		l = LineSegment3f( V3f( 0, -1, 0 ), V3f( 0, 1, 0 ) )
+		p = Plane3f( V3f( 0, 1, 0 ), 0 )
+		self.assertEqual( l.intersect( p ), ( True, V3f( 0, 0, 0 ) ) )
+		self.assertEqual( l.intersectT( p ), ( True, 0.5 ) )
+		
+		p = Plane3f( V3f( -1, 0, 0 ), 10 )
+		self.assertEqual( l.intersect( p )[0], False )
+		self.assertEqual( l.intersectT( p )[0], False )		
+							
 	def testRepr( self ) :
 	
 		p0 = V3f( 0, 0, 0 )
