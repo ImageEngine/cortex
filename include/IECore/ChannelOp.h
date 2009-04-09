00f43a4b2f66d6ba32a8dd73c68bafefc2c75867
Mark Williams <mark@image-engine.com>
2009-04-09 17:37:51 +0000

    Implemented todo

--- include/IECore/ChannelOp.h
+++ include/IECore/ChannelOp.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2008-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -57,11 +57,6 @@ class ChannelOp : public ImagePrimitiveOp
 	
 	protected :
 	
-		/// Implemented to call modifyChannels(). Derived classes should implement modifyChannels rather than
-		/// this function.
-		/// \todo Consider making this private to enforce the above statement.
-		virtual void modifyTypedPrimitive( ImagePrimitivePtr image, ConstCompoundObjectPtr operands );
-
 		typedef std::vector<DataPtr> ChannelVector;
 
 		/// Should be implemented by derived classes to modify the data in the passes channels in place.
@@ -77,6 +72,9 @@ class ChannelOp : public ImagePrimitiveOp
 		virtual void modifyChannels( const Imath::Box2i &displayWindow, const Imath::Box2i &dataWindow, ChannelVector &channels ) = 0;
 	
 	private :
+	
+		/// Implemented to call modifyChannels(). 
+		virtual void modifyTypedPrimitive( ImagePrimitivePtr image, ConstCompoundObjectPtr operands );
 	
 		StringVectorParameterPtr m_channelNamesParameter;
 				
