e024c9d73a941e02b9ff1b7430cf644e4ca126eb
John Haddon <john@image-engine.com>
2009-04-21 23:40:36 +0000

    Added a test which exposes an object identity problem. Even when we have a wrapped object (and it therefore has a mapping from RefCounted * to PyObject *) we don't get the original object back unless we return it to python via some pointer for which WrapperToPython has been called. If we return it via some other base class pointer then we lose out and get a different object.

--- test/IECore/WrapperToPython.py
+++ test/IECore/WrapperToPython.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -34,14 +34,14 @@
 
 import unittest
 
-from IECore import *
+import IECore
 
 class TestWrapperToPython( unittest.TestCase ) :
 
 	def test( self ) :
 	
-		f = FileSequenceParameter( "f", "d" )
-		c = CompoundParameter(
+		f = IECore.FileSequenceParameter( "f", "d" )
+		c = IECore.CompoundParameter(
 			members = [
 				f
 			]
@@ -50,6 +50,14 @@ class TestWrapperToPython( unittest.TestCase ) :
 		self.assert_( c["f"].isSame( f ) )
 		self.assert_( c["f"] is f )
 		self.assertEqual( c["f"], f )
+
+	def testReturningNonWrappedPtr( self ) :
+	
+		r = IECore.ReadProcedural()
+		o = IECore.CompoundObject( { "c" : r } )
+
+		self.assert_( o["c"].isSame( r ) )
+		self.assert_( o["c"] is r )
 
 if __name__ == "__main__":
         unittest.main()
