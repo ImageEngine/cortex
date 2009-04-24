0a9e5891a7aa3b5e976d32f681b199b29c75cad8
John Haddon <john@image-engine.com>
2009-04-24 19:30:56 +0000

    Added missing member pointers.

--- contrib/IECoreGL/include/IECoreGL/CameraController.h
+++ contrib/IECoreGL/include/IECoreGL/CameraController.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -51,6 +51,8 @@ class CameraController : public IECore::RefCounted
 {
 
 	public :
+
+		IE_CORE_DECLAREMEMBERPTR( CameraController );
 
 		CameraController( CameraPtr camera, float centreOfInterest = 5.0f );
 
--- contrib/IECoreGL/include/IECoreGL/SceneViewer.h
+++ contrib/IECoreGL/include/IECoreGL/SceneViewer.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -50,6 +50,8 @@ class SceneViewer : public Window
 {
 
 	public :
+	
+		IE_CORE_DECLAREMEMBERPTR( SceneViewer );
 	
 		/// Creates a new window with a view onto the
 		/// specified scene.
