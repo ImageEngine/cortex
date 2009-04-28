48a1734d6216c2a8ed0cdb0929fc82a729790f7e
Mark Williams <mark@image-engine.com>
2009-04-28 01:25:02 +0000

    Applied Windows build fix

--- test/IECore/RadixSortTest.h
+++ test/IECore/RadixSortTest.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2008-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -60,7 +60,7 @@ struct RadixSortTest
 	void test()
 	{
 		unsigned seed = 42;
-		boost::mt19937 generator( seed );
+		boost::mt19937 generator( static_cast<boost::mt19937::result_type>( seed ) );
 		
 		boost::uniform_real<> uni_dist( std::numeric_limits<T>::min(), std::numeric_limits<T>::max() );
 		boost::variate_generator<boost::mt19937&, boost::uniform_real<> > uni( generator, uni_dist );
--- test/IECore/SweepAndPruneTest.h
+++ test/IECore/SweepAndPruneTest.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2008-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -102,7 +102,7 @@ struct SweepAndPruneTest
 	void test()
 	{
 		unsigned seed = 42;
-		boost::mt19937 generator( seed );
+		boost::mt19937 generator( static_cast<boost::mt19937::result_type>( seed ) );
 		
 		typedef typename BoxTraits<T>::BaseType VecType;
 		typedef typename VectorTraits<VecType>::BaseType BaseType;
