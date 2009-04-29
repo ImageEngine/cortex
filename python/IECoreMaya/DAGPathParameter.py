4d7ebf7810aebdc3b99ef8c80e28230dd311e511
Mark Williams <mark@image-engine.com>
2009-04-29 17:03:55 +0000

    Fixed deprecation warnings

--- python/IECoreMaya/DAGPathParameter.py
+++ python/IECoreMaya/DAGPathParameter.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -167,4 +167,4 @@ class DAGPathParameter( IECore.StringParameter ):
 				raise Exception, "Node '%s' does not exist!" % dagNodePath
 			return None
 
-IECore.makeRunTimeTyped( DAGPathParameter, 350001, IECore.StringParameter )
+IECore.registerRunTimeTyped( DAGPathParameter, 350001, IECore.StringParameter )
--- python/IECoreMaya/NodeParameter.py
+++ python/IECoreMaya/NodeParameter.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2008-2009, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -160,4 +160,4 @@ class NodeParameter( IECore.StringParameter ):
 		list.getDependNode( 0, obj )
 		return obj
 
-IECore.makeRunTimeTyped( NodeParameter, 350002, IECore.StringParameter )
+IECore.registerRunTimeTyped( NodeParameter, 350002, IECore.StringParameter )
