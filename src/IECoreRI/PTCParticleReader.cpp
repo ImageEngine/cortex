d4f5c128df724b25f88cc18546029b58d7eda3ff
John Haddon <john@image-engine.com>
2009-05-15 21:11:42 +0000

    Updated copyright and removed todo i just did.

--- src/IECoreRI/PTCParticleReader.cpp
+++ src/IECoreRI/PTCParticleReader.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007-2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -48,7 +48,6 @@
 #include <algorithm>
 
 // \todo implement all attribute types from PTC definition.
-// \todo use Imath::Color instead of V3f for color attributes.
 
 #include <fstream>
 #include <cassert>
