d7f5e5086c2b9f6631669ffbf10ee3a2420135f1
Mark Williams <mark@image-engine.com>
2009-05-21 17:08:19 +0000

    Fixed deprecation warnings

--- test/IECoreTruelight/TruelightColorTransformOpTest.py
+++ test/IECoreTruelight/TruelightColorTransformOpTest.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2008-2009, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -42,10 +42,10 @@ class TruelightColorTransformOpTest( unittest.TestCase ) :
 	def testDefaultParameterValues( self ) :
 
 		o = TruelightColorTransformOp()
-		self.assertEqual( o.profile.getTypedValue(), "Kodak" )
-		self.assertEqual( o.display.getTypedValue(), "monitor" )
-		self.assertEqual( o.inputSpace.getCurrentPresetName(), "linear" )
-		self.assertEqual( o.rawTruelightOutput.getTypedValue(), True )
+		self.assertEqual( o["profile"].getTypedValue(), "Kodak" )
+		self.assertEqual( o["display"].getTypedValue(), "monitor" )
+		self.assertEqual( o["inputSpace"].getCurrentPresetName(), "linear" )
+		self.assertEqual( o["rawTruelightOutput"].getTypedValue(), True )
 
 	def testCommands( self ) :
 
