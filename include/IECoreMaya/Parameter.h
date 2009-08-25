a852dfe6735c7c6de52e4ca1454fc8acbd5b0b30
John Haddon <john@image-engine.com>
2009-08-25 21:40:07 +0000

    Added a todo.

--- include/IECoreMaya/Parameter.h
+++ include/IECoreMaya/Parameter.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -48,6 +48,7 @@ namespace IECoreMaya
 /// representation of IECore::Parameter instances as Maya
 /// dynamic attributes on a node.
 /// \todo Documentation!
+/// \todo I think this functionality should just be moved to the ParameterHandler class.
 class Parameter
 {
 
