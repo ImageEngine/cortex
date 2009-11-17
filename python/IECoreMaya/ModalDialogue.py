39641df64abf1803cdaa7dbf13bf81b4c08e40de
Tom Cowland <tomc@image-engine.com>
2009-11-17 19:18:42 +0000

    Improved supprt for returning Python types. It diverges slightly from standard maya behaviour, but gives greatly increased flexibility.

--- python/IECoreMaya/ModalDialogue.py
+++ python/IECoreMaya/ModalDialogue.py
@@ -32,6 +32,7 @@
 #
 ##########################################################################
 
+import IECore
 import IECoreMaya
 import maya.cmds
 
@@ -46,24 +47,53 @@ class ModalDialogue( IECoreMaya.UIElement ) :
 	def __init__( self ) :
 
 		IECoreMaya.UIElement.__init__( self, maya.cmds.setParent( query=True ) )
+		ModalDialogue.__returnObject = None;
 
 	## Should be called by derived classes when they wish to close their dialogue.
-	# The result will be returned to the caller of the run() method.
+	# The result will be returned to the caller of the run() method. This differs
+	# from the default maya behaviour for a layoutDialog, as we allow any python
+	# type to be returned. If the dialog is dismissed directly with the maya.cmd
+	# then that value will be returned instead.
 	def _dismiss( self, result ) :
 
-		maya.cmds.layoutDialog( dismiss = result )
+		ModalDialogue.__returnObject = result
+		maya.cmds.layoutDialog( dismiss = 'ModalDialogueReturnObject' )
 
-	## Call this method to open a dialogue - the return value is the string returned
-	# by the dialogue in its _dismiss method.
+	## Call this method to open a dialogue - the return value is the object supplied
+	# by the derived class, or the string supplied to layoutDialog( dismiss=%s ) if
+	# called directly. If the string 'None', 'True', or 'False' is passed to a
+	# direct call, then the equivalent python object is returned instead of a string.
 	@classmethod
-	def run( cls ) :
+	def run( cls, *args, **kwargs ) :
 
 		ModalDialogue.__toInstantiate = cls
+		ModalDialogue.__calltimeArgs = args
+		ModalDialogue.__calltimeKwargs = kwargs
+		
 		title = maya.mel.eval( 'interToUI( "%s" )' % cls.__name__ )
-		result = maya.cmds.layoutDialog( ui = 'import IECoreMaya; IECoreMaya.ModalDialogue._ModalDialogue__instantiate()', title=title )
-		return result
+		mayaResult = maya.cmds.layoutDialog( ui = 'import IECoreMaya; IECoreMaya.ModalDialogue._ModalDialogue__instantiate()', title = title )
+
+		if mayaResult == 'ModalDialogueReturnObject' :
+		
+			stopMeLeaking = ModalDialogue.__returnObject
+			ModalDialogue.__returnObject = None
+			return stopMeLeaking
+		
+		elif mayaResult == 'None' :
+			return None
+		
+		elif mayaResult == 'True' :
+			return True
+		
+		elif mayaResult == 'False' :
+			return False
+		
+		return mayaResult
 
 	@classmethod
 	def __instantiate( cls ) :
 
-		ModalDialogue.__currentDialogue = cls.__toInstantiate()
+		ModalDialogue.__currentDialogue = cls.__toInstantiate( *ModalDialogue.__calltimeArgs, **ModalDialogue.__calltimeKwargs )
+		ModalDialogue.__calltimeArgs = None
+		ModalDialogue.__calltimeKwargs = None
+
