6f3dc45ee259d0efc092475fb46ba13addc7d5a3
John Haddon <john@image-engine.com>
2010-02-10 01:28:04 +0000

    Added flush() method necessary for python2.6.

--- test/IECoreRI/All.py
+++ test/IECoreRI/All.py
@@ -80,5 +80,10 @@ class SplitStream :
 
 		sys.stderr.write( l )
 		self.__f.write( l )
-
+		
+	def flush( self ) :
+	
+		sys.stderr.flush()
+		self.__f.flush()
+		
 unittest.TestProgram( testRunner = unittest.TextTestRunner( stream = SplitStream(), verbosity = 2 ) )
