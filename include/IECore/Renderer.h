f42a7b5640eda039152045f1a701d9691628c4ca
John Haddon <john@image-engine.com>
2010-02-13 01:21:36 +0000

    Removing unnecessary include.

--- include/IECore/Renderer.h
+++ include/IECore/Renderer.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007-2009, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2010, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -39,7 +39,6 @@
 #include "IECore/PrimitiveVariable.h"
 #include "IECore/VectorTypedData.h"
 #include "IECore/CompoundData.h"
-#include "IECore/Parameterised.h"
 #include "IECore/CubicBasis.h"
 
 #include "OpenEXR/ImathMatrix.h"
