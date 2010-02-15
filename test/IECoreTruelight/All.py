5dc12ec7c51f054598dd61f486b36d9cba30cd17
John Haddon <john@image-engine.com>
2010-02-15 20:04:21 +0000

    Fixing SplitStream for python 2.6.

--- test/IECoreTruelight/All.py
+++ test/IECoreTruelight/All.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2008-2010, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -48,4 +48,9 @@ class SplitStream :
 		sys.stderr.write( l )
 		self.__f.write( l )
 
+	def flush( self ) :
+	
+		sys.stderr.flush()
+		self.__f.flush()
+		
 unittest.TestProgram( testRunner = unittest.TextTestRunner( stream = SplitStream(), verbosity = 2 ) )
