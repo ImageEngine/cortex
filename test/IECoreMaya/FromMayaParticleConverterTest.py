7c0e66054c7a52639270934557fc589a3825cfea
John Haddon <john@image-engine.com>
2010-03-16 18:42:47 +0000

    Fixing old skool parameter access.

--- test/IECoreMaya/FromMayaParticleConverterTest.py
+++ test/IECoreMaya/FromMayaParticleConverterTest.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2008-2010, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -75,7 +75,7 @@ class FromMayaParticleConverterTest( unittest.TestCase ) :
 
 		converter = IECoreMaya.FromMayaShapeConverter.create( str( particle ), IECore.PointsPrimitive.staticTypeId() )
 
-		converter.parameters().attributeNames = IECore.StringVectorData( [ "velocity", "mass" ] )
+		converter["attributeNames"] = IECore.StringVectorData( [ "velocity", "mass" ] )
 
 		self.assert_( converter.isInstanceOf( IECore.TypeId( IECoreMaya.TypeId.FromMayaParticleConverter ) ) )
 
