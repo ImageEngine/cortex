12e11aa2146d118c6d1b1cd11c8faa1b5738b8c2
John Haddon <john@image-engine.com>
2010-04-05 23:22:17 +0000

    Extended todo.

--- python/IECoreMaya/ParameterUI.py
+++ python/IECoreMaya/ParameterUI.py
@@ -62,7 +62,9 @@ class ParameterUI :
 	# and leave the parent layout unchanged on exit.
 	# \todo Document the expected behaviour of derived classes with respect to setting up self._layout,
 	# or provide a more explicit mechanism for the same thing.
-	# \todo Document the meaning of the various keyword arguments.
+	# \todo Document the meaning of the various keyword arguments - perhaps the names of these should be
+	# prefixed with the name of the class which implements each argument so as to make it easier to find
+	# the documentation too.
 	def __init__( self, parameterisedHolderNode, parameter, **kw ) :
 
 		self.__node = parameterisedHolderNode
