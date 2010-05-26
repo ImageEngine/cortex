b6e26993f98a1077064a1c7f5915dcaa00f2cc03
Tom Cowland <tomc@image-engine.com>
2010-05-26 19:57:11 +0000

    Adds doRenderState to prevent copious errors in FnProceduralHolderTest.

--- test/IECoreMaya/FnProceduralHolderTest.py
+++ test/IECoreMaya/FnProceduralHolderTest.py
@@ -67,6 +67,10 @@ class FnProceduralHolderTest( IECoreMaya.TestCase ) :
 		def doRender( self, renderer, args ) :
 			
 			renderer.sphere( args["radius"].value, -1, 1, 360, {} )
+			
+		def doRenderState( self, renderer, args ) :
+			
+			pass
 
 	def testScene( self ) :
 
