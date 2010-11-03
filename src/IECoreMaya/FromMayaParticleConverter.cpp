2e0a9294a953b37009031953f1ef25957f1b0b72
John Haddon <john@image-engine.com>
2010-11-03 18:52:29 +0000

    Fixing indentation.

--- src/IECoreMaya/FromMayaParticleConverter.cpp
+++ src/IECoreMaya/FromMayaParticleConverter.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2008-2010, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -129,9 +129,9 @@ IECore::PrimitivePtr FromMayaParticleConverter::doPrimitiveConversion( MFnPartic
 	MVectorArray position;
 	fnParticle.position( position );
 
-        IECore::V3fVectorDataPtr pos = new IECore::V3fVectorData();
-        pos->writable().resize( position.length() );
-        std::transform( MArrayIter<MVectorArray>::begin( position ), MArrayIter<MVectorArray>::end( position ), pos->writable().begin(), IECore::VecConvert<MVector, V3f>() );
+	IECore::V3fVectorDataPtr pos = new IECore::V3fVectorData();
+	pos->writable().resize( position.length() );
+	std::transform( MArrayIter<MVectorArray>::begin( position ), MArrayIter<MVectorArray>::end( position ), pos->writable().begin(), IECore::VecConvert<MVector, V3f>() );
 	points->variables["P"] = IECore::PrimitiveVariable( IECore::PrimitiveVariable::Vertex, pos );
 
 	const IECore::StringVectorParameter::ValueType &attributeNames = attributeNamesParameter()->getTypedValue();
