01da8109c9526f584d6216f00e0591d99a1b0155
John Haddon <john@image-engine.com>
2011-03-18 21:06:51 +0000

    Fixed syntax error.

--- test/IECore/ops/maths/multiply/multiply-1.py
+++ test/IECore/ops/maths/multiply/multiply-1.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2007-2010, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2007-2011, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -68,6 +68,6 @@ class multiply( Op ) :
 
 	def doOperation( self, operands ) :
 
-		return IECore.IntData( operands["a"].value * operands["b"].value )
+		return IntData( operands["a"].value * operands["b"].value )
 
 registerRunTimeTyped( multiply )
