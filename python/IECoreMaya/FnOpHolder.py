16f7e28e571e3daae7cbf5f61866f1d649052e67
John Haddon <john@image-engine.com>
2011-05-10 18:52:42 +0000

    IECoreMaya.FnOpHolder.create() now loads the default version for the op if no version is specified.

--- python/IECoreMaya/FnOpHolder.py
+++ python/IECoreMaya/FnOpHolder.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2008, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2008-2011, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -45,7 +45,7 @@ class FnOpHolder( FnParameterisedHolder ) :
 	## Creates a new node holding a new instance of the op of the specified
 	# type and version. Returns an FnOpHolder instance attached to this node.
 	@staticmethod
-	def create( nodeName, opType, opVersion ) :
+	def create( nodeName, opType, opVersion=None ) :
 
 		holder = maya.cmds.createNode( "ieOpHolderNode", name=nodeName, skipSelect=True )
 
