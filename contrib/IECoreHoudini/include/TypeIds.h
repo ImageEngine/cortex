102237c4da1a070d40f83e0275810de1f33f1672
Andrew Kaufman <andrewk.imageengine@gmail.com>
2011-09-29 23:37:54 +0000

    adding type ids for forthcoming classes

--- contrib/IECoreHoudini/include/TypeIds.h
+++ contrib/IECoreHoudini/include/TypeIds.h
@@ -3,7 +3,7 @@
 //  Copyright 2010 Dr D Studios Pty Limited (ACN 127 184 954) (Dr. D Studios),
 //  its affiliates and/or its licensors.
 //
-//  Copyright (c) 2010, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2010-2011, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -73,6 +73,8 @@ namespace IECoreHoudini
 		ToHoudiniGroupConverterTypeId = 111026,
 		ToHoudiniStringDetailAttribConverterTypeId = 111027,
 		ToHoudiniStringVectorAttribConverterTypeId = 111028,
+		RATDeepImageReaderTypeId = 111029,
+		RATDeepImageWriterTypeId = 111030,
 		// remember to update TypeIdBinding.cpp
 		LastTypeId = 111999,
 	};
