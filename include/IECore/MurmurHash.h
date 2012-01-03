bbdf312745ce0243da234573ca5d79f8bc3db063
John Haddon <thehaddonyoof@gmail.com>
2012-01-03 20:01:01 +0000

    Fix for compilation with gcc 4.4.5 on linux.

--- include/IECore/MurmurHash.h
+++ include/IECore/MurmurHash.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2011, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2011-2012, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -35,6 +35,7 @@
 #ifndef IECORE_MURMURHASH_H
 #define IECORE_MURMURHASH_H
 
+#include <stdint.h>
 #include <iostream>
 
 #include "OpenEXR/ImathMatrix.h"
