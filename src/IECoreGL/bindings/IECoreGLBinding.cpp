a120db55a5390e4580457eabaca2c231744a038e
John Haddon <thehaddonyoof@gmail.com>
2012-01-24 10:14:39 +0000

    Adding missing changes which should have been in revision 4552.

--- src/IECoreGL/bindings/IECoreGLBinding.cpp
+++ src/IECoreGL/bindings/IECoreGLBinding.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007-2010, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2012, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -66,6 +66,7 @@
 #include "IECoreGL/bindings/ToGLTextureConverterBinding.h"
 #include "IECoreGL/bindings/PrimitiveBinding.h"
 #include "IECoreGL/bindings/PointsPrimitiveBinding.h"
+#include "IECoreGL/bindings/SelectorBinding.h"
 
 
 using namespace IECoreGL;
@@ -103,6 +104,7 @@ BOOST_PYTHON_MODULE( _IECoreGL )
 	bindToGLTextureConverter();
 	bindPrimitive();
 	bindPointsPrimitive();
+	bindSelector();
 
 	def( "init", &IECoreGL::init );
 }
