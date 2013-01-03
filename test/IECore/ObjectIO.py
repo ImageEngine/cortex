cecc7e05600c0c52d3d7d2ee42817417f9196c8d
Andrew Kaufman <andrewk.imageengine@gmail.com>
2013-01-03 19:32:54 +0000

    cleaning up in tearDown

--- test/IECore/ObjectIO.py
+++ test/IECore/ObjectIO.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#  Copyright (c) 2007-2012, Image Engine Design Inc. All rights reserved.
+#  Copyright (c) 2007-2013, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -261,7 +261,7 @@ class TestObjectIO( unittest.TestCase ) :
 
 	def tearDown( self ) :
 
-		for f in [ "test/o.fio" ] :
+		for f in [ "test/o.fio", "test/FileIndexedIOSlashes.fio" ] :
 			if os.path.isfile( f ) :
 				os.remove( f )
 
