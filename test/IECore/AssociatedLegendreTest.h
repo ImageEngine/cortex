1952afba902d14f079c03e7ecb7134cfaca047d0
Ben Toogood <bent@image-engine.com>
2013-07-23 17:35:19 -0700

    Fixed AssociatedLegendreTest for GCC 4.7.3 on Ubuntu.

--- test/IECore/AssociatedLegendreTest.h
+++ test/IECore/AssociatedLegendreTest.h
@@ -49,8 +49,8 @@ class AssociatedLegendreTest
 {
 	public:
 
-		static void testEvaluation();
-		static void testDepthEvaluation();
+		void testEvaluation();
+		void testDepthEvaluation();
 
 	private:
 
@@ -70,8 +70,8 @@ struct AssociatedLegendreTestSuite : public boost::unit_test::test_suite
 	{
 		static boost::shared_ptr< AssociatedLegendreTest< T > > instance(new AssociatedLegendreTest<T>());
 
-		add( BOOST_CLASS_TEST_CASE( &(AssociatedLegendreTest< T >::testEvaluation), instance ) );
-		add( BOOST_CLASS_TEST_CASE( &(AssociatedLegendreTest< T >::testDepthEvaluation), instance ) );
+		add( BOOST_CLASS_TEST_CASE( &AssociatedLegendreTest< T >::testEvaluation, instance ) );
+		add( BOOST_CLASS_TEST_CASE( &AssociatedLegendreTest< T >::testDepthEvaluation, instance ) );
 	}
 
 };
