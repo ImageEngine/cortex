3bda3cdc4069411fcec8848df577d471f3255487
goddardl <lukeg@image-engine.com>
2013-08-01 14:52:57 -0700

    Added an optional argument to the IECoreGL::ToGLTextureConverter constructor that forces the creation of missing RGB channels rather than just throwing an exception. Updated the bindings for the above. Fixed typos.

--- src/IECoreGL/bindings/ToGLTextureConverterBinding.cpp
+++ src/IECoreGL/bindings/ToGLTextureConverterBinding.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2009-2010, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2009-2013, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -49,7 +49,9 @@ void bindToGLTextureConverter()
 {
 	IECorePython::RunTimeTypedClass<ToGLTextureConverter>()
 		.def( init< IECore::ConstObjectPtr >() )
+		.def( init< IECore::ConstObjectPtr, bool >() )
 	;
 }
 
 }
+
