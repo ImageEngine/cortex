01adc4292f1d0f1120bf32805a99f69aa6b8999b
davidsminor <davidm@image-engine.com>
2013-09-18 10:02:55 -0700

    moved some checks into computeMinMaxVertsPerFace(), which now gets called by setTopology()

--- src/IECore/MeshPrimitive.cpp
+++ src/IECore/MeshPrimitive.cpp
@@ -84,13 +84,25 @@ void MeshPrimitive::computeMinMaxVertsPerFace() const
 {
 	int minVertsPerFace = 0x7fffffff;
 	int maxVertsPerFace = 0;
+	unsigned int numExpectedVertexIds = 0;
 	for ( vector<int>::const_iterator it = m_verticesPerFace->readable().begin(); it != m_verticesPerFace->readable().end(); it++ )
 	{
 		int vertsPerFace = *it;
 		minVertsPerFace = std::min( minVertsPerFace, vertsPerFace );
 		maxVertsPerFace = std::max( maxVertsPerFace, vertsPerFace );
+		numExpectedVertexIds += vertsPerFace;
 	}
-	m_minVerticesPerFace = minVertsPerFace;
+	if( minVertsPerFace<3 )
+	{
+		throw Exception( "Bad topology - number of vertices per face less than 3." );
+	}
+
+	if( numExpectedVertexIds!=m_vertexIds->readable().size() )
+	{
+		throw Exception( "Bad topology - number of vertexIds not equal to sum of verticesPerFace" );
+	}
+
+	m_minVerticesPerFace = ( m_verticesPerFace->readable().size() ? minVertsPerFace : 0 );
 	m_maxVerticesPerFace = maxVertsPerFace;
 }
 		
@@ -138,30 +150,11 @@ void MeshPrimitive::setTopology( ConstIntVectorDataPtr verticesPerFace, ConstInt
 		maxVertexId = std::max( maxVertexId, id );
 	}
 	
-	int minVertsPerFace = 0x7fffffff;
-	int maxVertsPerFace = 0;
-	unsigned int numExpectedVertexIds = 0;
-	for ( vector<int>::const_iterator it = verticesPerFace->readable().begin(); it != verticesPerFace->readable().end(); it++ )
-	{
-		int vertsPerFace = *it;
-		minVertsPerFace = std::min( minVertsPerFace, vertsPerFace );
-		maxVertsPerFace = std::max( maxVertsPerFace, vertsPerFace );
-		numExpectedVertexIds += vertsPerFace;
-	}
-	if( minVertsPerFace<3 )
-	{
-		throw Exception( "Bad topology - number of vertices per face less than 3." );
-	}
-
-	if( numExpectedVertexIds!=vertexIds->readable().size() )
-	{
-		throw Exception( "Bad topology - number of vertexIds not equal to sum of verticesPerFace" );
-	}
-
-	m_minVerticesPerFace = ( verticesPerFace->readable().size() ? minVertsPerFace : 0 );
-	m_maxVerticesPerFace = maxVertsPerFace;
 	m_verticesPerFace = verticesPerFace->copy();
 	m_vertexIds = vertexIds->copy();
+	
+	computeMinMaxVertsPerFace();
+	
 	if( m_vertexIds->readable().size() )
 	{
 		m_numVertices = 1 + maxVertexId;
