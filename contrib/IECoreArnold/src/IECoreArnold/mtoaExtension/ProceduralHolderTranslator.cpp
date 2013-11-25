9368dfbc56c04fc9df11eaa83002f9ad521dc486
Ashley Retallack <ashley-r@blue-bolt.com>
2013-11-25 15:00:53 +0000

    added fixes for instnace not working in MtoA 0.24.0+

--- contrib/IECoreArnold/src/IECoreArnold/mtoaExtension/ProceduralHolderTranslator.cpp
+++ contrib/IECoreArnold/src/IECoreArnold/mtoaExtension/ProceduralHolderTranslator.cpp
@@ -45,6 +45,10 @@
 #include "extension/Extension.h"
 #include "translators/shape/ShapeTranslator.h"
 
+#ifdef MTOA_VERSION_HEADER // only exists in mtoa 0.24+
+#include "utils/Version.h"
+#endif
+
 #include "IECore/CompoundParameter.h"
 
 #include "IECoreMaya/ProceduralHolder.h"
@@ -53,25 +57,31 @@
 class ProceduralHolderTranslator : public CShapeTranslator
 {
 
-	public :
-	
-		virtual AtNode *CreateArnoldNodes()
-		{
+    public :
+    
+        virtual AtNode *CreateArnoldNodes()
+        {
+            #ifdef MTOA_VERSION_HEADER            
+                  m_isMasterDag =  IsMasterInstance();
+                  m_masterDag = GetMasterInstance();
+            #else
                   m_isMasterDag =  IsMasterInstance(m_masterDag);
+            #endif
 
-                  if (m_isMasterDag)
-                  {
+            if (m_isMasterDag)
+            {
+                AtNode * tmpRes = AddArnoldNode("procedural");
+                return tmpRes;
+            }
+            else
+            {
+                AtNode * tmpRes = AddArnoldNode("ginstance");
+                return tmpRes;
+            }
+        }
+        
 
-			return AddArnoldNode( "procedural" );
-                  }
-                  else
-                  {
-                        return AddArnoldNode( "ginstance" );
-                  }
-		}
-		
-
-		virtual void Export( AtNode *node )
+        virtual void Export( AtNode *node )
                 {
                   const char* nodeType = AiNodeEntryGetName(AiNodeGetNodeEntry(node));
                   if (strcmp(nodeType, "ginstance") == 0)
@@ -84,235 +94,235 @@ class ProceduralHolderTranslator : public CShapeTranslator
                   }
                 }
 
-                virtual AtNode* ExportInstance(AtNode *instance, const MDagPath& masterInstance)
+        virtual AtNode* ExportInstance(AtNode *instance, const MDagPath& masterInstance)
+        {
+           AtNode* masterNode = AiNodeLookUpByName(masterInstance.partialPathName().asChar());
+
+
+           int instanceNum = m_dagPath.instanceNumber();
+
+           if ( instanceNum > 0 )
+             {
+               AiNodeSetStr(instance, "name", m_dagPath.partialPathName().asChar());
+
+               ExportMatrix(instance, 0);
+
+               AiNodeSetPtr(instance, "node", masterNode);
+               AiNodeSetBool(instance, "inherit_xform", false);
+               int visibility = AiNodeGetInt(masterNode, "visibility");
+               AiNodeSetInt(instance, "visibility", visibility);
+
+               AtNode *shader = arnoldShader();
+               if( shader )
+               {
+                   AiNodeSetPtr( instance, "shader", shader );
+               }
+
+               // Export light linking per instance
+               ExportLightLinking(instance);
+             }
+           return instance;
+        }
+
+        virtual void ExportProcedural( AtNode *node )
+        {
+            // do basic node export
+            
+            ExportMatrix( node, 0 );
+            
+            AtNode *shader = arnoldShader();
+            if( shader )
+            {
+                AiNodeSetPtr( node, "shader", shader );
+            }
+            
+            AiNodeSetInt( node, "visibility", ComputeVisibility() );
+            
+            MPlug plug = FindMayaObjectPlug( "receiveShadows" );
+            if( !plug.isNull() )
+            {
+                AiNodeSetBool( node, "receive_shadows", plug.asBool() );
+            }
+            
+            plug = FindMayaObjectPlug( "aiSelfShadows" );
+            if( !plug.isNull() )
+            {
+                AiNodeSetBool( node, "self_shadows", plug.asBool() );
+            }
+            
+            plug = FindMayaObjectPlug( "aiOpaque" );
+            if( !plug.isNull() )
+            {
+                AiNodeSetBool( node, "opaque", plug.asBool() );
+            }
+            
+            // export any shading groups or displacement shaders which look like they
+            // may be connected to procedural parameters. this ensures that maya shaders
+            // the procedural will expect to find at rendertime will be exported to the
+            // ass file (they otherwise might not be if they're not assigned to any objects).
+            
+            exportShadingInputs();
+            
+            // now set the procedural-specific parameters
+            
+            MFnDagNode fnDagNode( m_dagPath );
+            MBoundingBox bound = fnDagNode.boundingBox();
+            
+            AiNodeSetPnt( node, "min", bound.min().x, bound.min().y, bound.min().z );
+            AiNodeSetPnt( node, "max", bound.max().x, bound.max().y, bound.max().z );
+            
+            const char *dsoPath = getenv( "IECOREARNOLD_PROCEDURAL_PATH" );
+            AiNodeSetStr( node, "dso", dsoPath ? dsoPath : "ieProcedural.so" );
+            
+            AiNodeDeclare( node, "className", "constant STRING" );
+            AiNodeDeclare( node, "classVersion", "constant INT" );
+            AiNodeDeclare( node, "parameterValues", "constant ARRAY STRING" );
+            
+            // cast should be ok as we're registered to only work on procedural holders
+            IECoreMaya::ProceduralHolder *pHolder = static_cast<IECoreMaya::ProceduralHolder *>( fnDagNode.userNode() );
+            
+            std::string className;
+            int classVersion;
+            IECore::ParameterisedProceduralPtr procedural = pHolder->getProcedural( &className, &classVersion );
+            
+            AiNodeSetStr( node, "className", className.c_str() );
+            AiNodeSetInt( node, "classVersion", classVersion );
+            
+            IECorePython::ScopedGILLock gilLock;
+            try
+            {
+                boost::python::object parser = IECoreMaya::PythonCmd::globalContext()["IECore"].attr( "ParameterParser" )();
+                boost::python::object serialised = parser.attr( "serialise" )( procedural->parameters() );
+                
+                size_t numStrings = IECorePython::len( serialised );
+                AtArray *stringArray = AiArrayAllocate( numStrings, 1, AI_TYPE_STRING );
+                for( size_t i=0; i<numStrings; i++ )
                 {
-                   AtNode* masterNode = AiNodeLookUpByName(masterInstance.partialPathName().asChar());
-
-
-                   int instanceNum = m_dagPath.instanceNumber();
-
-                   if ( instanceNum > 0 )
-                     {
-                       AiNodeSetStr(instance, "name", m_dagPath.partialPathName().asChar());
-
-                       ExportMatrix(instance, 0);
-
-                       AiNodeSetPtr(instance, "node", masterNode);
-                       AiNodeSetBool(instance, "inherit_xform", false);
-                       int visibility = AiNodeGetInt(masterNode, "visibility");
-                       AiNodeSetInt(instance, "visibility", visibility);
-
-                       AtNode *shader = arnoldShader();
-                       if( shader )
-                       {
-                           AiNodeSetPtr( instance, "shader", shader );
-                       }
-
-                       // Export light linking per instance
-                       ExportLightLinking(instance);
-                     }
-                   return instance;
+                    std::string s = boost::python::extract<std::string>( serialised[i] );
+                    // hack to workaround ass parsing errors
+                    /// \todo Remove when we get the Arnold version that fixes this
+                    for( size_t c = 0; c<s.size(); c++ )
+                    {
+                        if( s[c] == '#' )
+                        {
+                            s[c] = '@';
+                        }
+                    }
+                    AiArraySetStr( stringArray, i, s.c_str() );
                 }
+                
+                AiNodeSetArray( node, "parameterValues", stringArray );
+            }
+            catch( boost::python::error_already_set )
+            {
+                PyErr_Print();
+            }
+        
+        }
 
-		virtual void ExportProcedural( AtNode *node )
-		{
-			// do basic node export
-			
-			ExportMatrix( node, 0 );
-			
-			AtNode *shader = arnoldShader();
-			if( shader )
-			{
-				AiNodeSetPtr( node, "shader", shader );
-			}
-			
-			AiNodeSetInt( node, "visibility", ComputeVisibility() );
-			
-			MPlug plug = FindMayaObjectPlug( "receiveShadows" );
-			if( !plug.isNull() )
-			{
-				AiNodeSetBool( node, "receive_shadows", plug.asBool() );
-			}
-			
-			plug = FindMayaObjectPlug( "aiSelfShadows" );
-			if( !plug.isNull() )
-			{
-				AiNodeSetBool( node, "self_shadows", plug.asBool() );
-			}
-			
-			plug = FindMayaObjectPlug( "aiOpaque" );
-			if( !plug.isNull() )
-			{
-				AiNodeSetBool( node, "opaque", plug.asBool() );
-			}
-			
-			// export any shading groups or displacement shaders which look like they
-			// may be connected to procedural parameters. this ensures that maya shaders
-			// the procedural will expect to find at rendertime will be exported to the
-			// ass file (they otherwise might not be if they're not assigned to any objects).
-			
-			exportShadingInputs();
-			
-			// now set the procedural-specific parameters
-			
-			MFnDagNode fnDagNode( m_dagPath );
-			MBoundingBox bound = fnDagNode.boundingBox();
-			
-			AiNodeSetPnt( node, "min", bound.min().x, bound.min().y, bound.min().z );
-			AiNodeSetPnt( node, "max", bound.max().x, bound.max().y, bound.max().z );
-			
-			const char *dsoPath = getenv( "IECOREARNOLD_PROCEDURAL_PATH" );
-			AiNodeSetStr( node, "dso", dsoPath ? dsoPath : "ieProcedural.so" );
-			
-			AiNodeDeclare( node, "className", "constant STRING" );
-			AiNodeDeclare( node, "classVersion", "constant INT" );
-			AiNodeDeclare( node, "parameterValues", "constant ARRAY STRING" );
-			
-			// cast should be ok as we're registered to only work on procedural holders
-			IECoreMaya::ProceduralHolder *pHolder = static_cast<IECoreMaya::ProceduralHolder *>( fnDagNode.userNode() );
-			
-			std::string className;
-			int classVersion;
-			IECore::ParameterisedProceduralPtr procedural = pHolder->getProcedural( &className, &classVersion );
-			
-			AiNodeSetStr( node, "className", className.c_str() );
-			AiNodeSetInt( node, "classVersion", classVersion );
-			
-			IECorePython::ScopedGILLock gilLock;
-			try
-			{
-				boost::python::object parser = IECoreMaya::PythonCmd::globalContext()["IECore"].attr( "ParameterParser" )();
-				boost::python::object serialised = parser.attr( "serialise" )( procedural->parameters() );
-				
-				size_t numStrings = IECorePython::len( serialised );
-				AtArray *stringArray = AiArrayAllocate( numStrings, 1, AI_TYPE_STRING );
-				for( size_t i=0; i<numStrings; i++ )
-				{
-					std::string s = boost::python::extract<std::string>( serialised[i] );
-					// hack to workaround ass parsing errors
-					/// \todo Remove when we get the Arnold version that fixes this
-					for( size_t c = 0; c<s.size(); c++ )
-					{
-						if( s[c] == '#' )
-						{
-							s[c] = '@';
-						}
-					}
-					AiArraySetStr( stringArray, i, s.c_str() );
-				}
-				
-				AiNodeSetArray( node, "parameterValues", stringArray );
-			}
-			catch( boost::python::error_already_set )
-			{
-				PyErr_Print();
-			}
-		
-		}
+        virtual bool RequiresMotionData()
+        {
+            return IsMotionBlurEnabled( MTOA_MBLUR_OBJECT ) && IsLocalMotionBlurEnabled();
+        }
+        
+        virtual void ExportMotion( AtNode *node, uint step )
+        {
+            if( !IsMotionBlurEnabled() )
+            {
+                return;
+            }
+            
+            ExportMatrix( node, step );
+        }
+        
+        static void nodeInitialiser( CAbTranslator context )
+        {
+            CExtensionAttrHelper helper( context.maya, "procedural" );
+            MakeArnoldVisibilityFlags( helper );
+            
+            helper.MakeInput( "self_shadows" );
+            helper.MakeInput( "opaque" );
 
-		virtual bool RequiresMotionData()
-		{
-			return IsMotionBlurEnabled( MTOA_MBLUR_OBJECT ) && IsLocalMotionBlurEnabled();
-		}
-		
-		virtual void ExportMotion( AtNode *node, AtUInt step )
-		{
-   			if( !IsMotionBlurEnabled() )
-			{
-				return;
-			}
-			
-			ExportMatrix( node, step );
-		}
-		
-		static void nodeInitialiser( CAbTranslator context )
-		{
-			CExtensionAttrHelper helper( context.maya, "procedural" );
-			MakeArnoldVisibilityFlags( helper );
-			
-			helper.MakeInput( "self_shadows" );
-			helper.MakeInput( "opaque" );
+        }
 
-		}
-
-		static void *creator()
-		{
-			return new ProceduralHolderTranslator();
-		}
-	
-	protected :
-	
-		/// Returns the arnold shader to assign to the procedural.
-		AtNode *arnoldShader()
-		{	
-			bool overrideShaders = false;
-			MPlug plug = FindMayaObjectPlug( "overrideProceduralShaders" );
-			if( !plug.isNull() )
-			{
-				// if we've been told explicitly not to override the shaders
-				// in the procedurals, then early out.
-				overrideShaders = plug.asBool();
-				if( !overrideShaders )
-				{
-					return 0;
-				}
-			}
-			
-			unsigned instNumber = m_dagPath.isInstanced() ? m_dagPath.instanceNumber() : 0;
-			MPlug shadingGroupPlug = GetNodeShadingGroup(m_dagPath.node(), instNumber);
-			
-			if( !overrideShaders )
-			{
-				// if we weren't explicitly told to override the shaders, then
-				// decide whether to or not based on whether a non-default
-				// shader has been applied to the shape by the user.
-				MObject shadingGroupNode = shadingGroupPlug.node();
-				MFnDependencyNode fnShadingGroupNode( shadingGroupNode );
-				if( fnShadingGroupNode.name() != "initialShadingGroup" )
-				{
-					overrideShaders = true;
-				}
-			}
-			
-			if( overrideShaders )
-			{
-				return ExportNode( shadingGroupPlug );
-			}
-			else
-			{
-				return 0;
-			}
-		}
-		
-		void exportShadingInputs()
-		{
-			MObject proceduralNode = m_dagPath.node();
-			MPlug nullPlug;
-			
-			MIteratorType filter;
-			MIntArray filterTypes;
-			filterTypes.append( MFn::kShadingEngine );
-			filterTypes.append( MFn::kDisplacementShader );			
-			filter.setFilterList( filterTypes );
-			
-			MItDependencyGraph itDG( proceduralNode, nullPlug, filter, MItDependencyGraph::kUpstream );
-			while( !itDG.isDone() )
-			{
-				MObject node = itDG.currentItem();
-				MFnDependencyNode fnNode( node );
-				MPlug plug;
-				if( fnNode.typeName() == "displacementShader" )
-				{
-					plug = fnNode.findPlug( "displacement" );
-				}
-				else
-				{
-					plug = fnNode.findPlug( "dsm" );
-				}
-				ExportNode( plug );
-				itDG.next();
-			}
-		}
+        static void *creator()
+        {
+            return new ProceduralHolderTranslator();
+        }
+    
+    protected :
+    
+        /// Returns the arnold shader to assign to the procedural.
+        AtNode *arnoldShader()
+        {   
+            bool overrideShaders = false;
+            MPlug plug = FindMayaObjectPlug( "overrideProceduralShaders" );
+            if( !plug.isNull() )
+            {
+                // if we've been told explicitly not to override the shaders
+                // in the procedurals, then early out.
+                overrideShaders = plug.asBool();
+                if( !overrideShaders )
+                {
+                    return 0;
+                }
+            }
+            
+            unsigned instNumber = m_dagPath.isInstanced() ? m_dagPath.instanceNumber() : 0;
+            MPlug shadingGroupPlug = GetNodeShadingGroup(m_dagPath.node(), instNumber);
+            
+            if( !overrideShaders )
+            {
+                // if we weren't explicitly told to override the shaders, then
+                // decide whether to or not based on whether a non-default
+                // shader has been applied to the shape by the user.
+                MObject shadingGroupNode = shadingGroupPlug.node();
+                MFnDependencyNode fnShadingGroupNode( shadingGroupNode );
+                if( fnShadingGroupNode.name() != "initialShadingGroup" )
+                {
+                    overrideShaders = true;
+                }
+            }
+            
+            if( overrideShaders )
+            {
+                return ExportNode( shadingGroupPlug );
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+        void exportShadingInputs()
+        {
+            MObject proceduralNode = m_dagPath.node();
+            MPlug nullPlug;
+            
+            MIteratorType filter;
+            MIntArray filterTypes;
+            filterTypes.append( MFn::kShadingEngine );
+            filterTypes.append( MFn::kDisplacementShader );         
+            filter.setFilterList( filterTypes );
+            
+            MItDependencyGraph itDG( proceduralNode, nullPlug, filter, MItDependencyGraph::kUpstream );
+            while( !itDG.isDone() )
+            {
+                MObject node = itDG.currentItem();
+                MFnDependencyNode fnNode( node );
+                MPlug plug;
+                if( fnNode.typeName() == "displacementShader" )
+                {
+                    plug = fnNode.findPlug( "displacement" );
+                }
+                else
+                {
+                    plug = fnNode.findPlug( "dsm" );
+                }
+                ExportNode( plug );
+                itDG.next();
+            }
+        }
 
 
         protected :
@@ -326,13 +336,13 @@ extern "C"
 
 DLLEXPORT void initializeExtension( CExtension &extension )
 {
-	extension.Requires( "ieCore" );
-	extension.RegisterTranslator(
-		"ieProceduralHolder",
-		"",
-		ProceduralHolderTranslator::creator,
-		ProceduralHolderTranslator::nodeInitialiser
-	);
+    extension.Requires( "ieCore" );
+    extension.RegisterTranslator(
+        "ieProceduralHolder",
+        "",
+        ProceduralHolderTranslator::creator,
+        ProceduralHolderTranslator::nodeInitialiser
+    );
 }
 
 DLLEXPORT void deinitializeExtension( CExtension &extension )
