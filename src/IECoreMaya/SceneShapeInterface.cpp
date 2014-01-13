d45fe41a2c8c32b4efebdb198159d15bfa3da3f7
Valerie Bernard <valerieb@image-engine.com>
2014-01-13 11:00:54 -0800

    Setting gl:curvesPrimitive:useGLLines attribute earlier so curve primitives are also affected

--- src/IECoreMaya/SceneShapeInterface.cpp
+++ src/IECoreMaya/SceneShapeInterface.cpp
@@ -1121,7 +1121,8 @@ void SceneShapeInterface::recurseBuildScene( IECoreGL::Renderer * renderer, cons
 	subSceneInterface->path( pathName );
 	std::string pathStr = relativePathName( pathName );
 	renderer->setAttribute( "name", new StringData( pathStr ) );
-
+	renderer->setAttribute( "gl:curvesPrimitive:useGLLines", new BoolData( true ) );
+	
 	if(pathStr != "/")
 	{
 		// Path space
@@ -1162,9 +1163,6 @@ void SceneShapeInterface::recurseBuildScene( IECoreGL::Renderer * renderer, cons
 
 	if( drawBounds && pathStr != "/" )
 	{
-		AttributeBlock aBox(renderer);
-		renderer->setAttribute( "gl:curvesPrimitive:useGLLines", new BoolData( true ) );
-
 		Box3d b = subSceneInterface->readBound( time );
 		Box3f bbox( b.min, b.max );
 		if( !bbox.isEmpty() )
