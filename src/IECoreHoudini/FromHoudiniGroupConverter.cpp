5bd20fbc6284f1f3caa55b8d386116be7d20a829
Andrew Kaufman <andrewk.imageengine@gmail.com>
2014-05-13 15:15:15 -0700

    Fixed bug in CortexConverter SOP todo with multiple CortexObject primitives with the same name.
    This was actually a bug in the DetailSplitter caching mechanism, which was failing to increment the MetaCount on GU_Details that it authored during validation.

--- src/IECoreHoudini/FromHoudiniGroupConverter.cpp
+++ src/IECoreHoudini/FromHoudiniGroupConverter.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2010-2013, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2010-2014, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -343,6 +343,7 @@ void FromHoudiniGroupConverter::doUnnamedConversion( const GU_Detail *geo, Group
 			GU_Detail *newGeo = new GU_Detail();
 			GA_Range thisPrim( geo->getPrimitiveMap(), offsets );
 			newGeo->mergePrimitives( *geo, thisPrim );
+			newGeo->incrementMetaCacheCount();
 			ObjectPtr object = doDetailConversion( newGeo, operands );
 			if ( VisibleRenderablePtr renderable = IECore::runTimeCast<VisibleRenderable>( object ) )
 			{
