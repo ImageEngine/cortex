e2b783907a4c8264090466def8e4466b218108d4
Andrew Kaufman <andrewk.imageengine@gmail.com>
2014-05-12 17:33:24 -0700

    Adding extra catch to SceneCacheNode::scene just in case.
    This function is used from every SceneCacheNode in Houdini to get the SceneInterface, so we want to be as careful as possible. Note that this still suffers from Memory Allocation Errors, which seems to be somehow controlled by Houdini directly using UT_NoMemHandler...

--- src/IECoreHoudini/SceneCacheNode.cpp
+++ src/IECoreHoudini/SceneCacheNode.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2013, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2013-2014, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -651,6 +651,10 @@ ConstSceneInterfacePtr SceneCacheNode<BaseType>::scene( const std::string &fileN
 	catch ( std::exception &e )
 	{
 		std::cerr << "Error loading \"" << fileName << "\" at location \"" << path << "\": " << e.what() << std::endl;
+	}
+	catch ( ... )
+	{
+		std::cerr << "Unknown error loading \"" << fileName << "\" at location \"" << path << "\": " << std::endl;
 	}
 	
 	return result;
