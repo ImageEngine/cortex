ImageEngine/cortex

commit 030e7c936de091e22564acec4bbffcb222154286

Adding thread-safe attribute read to SceneCache
This problem is mainly detected when the root location of scene caches have attributes. The root scene caches are returned by our SharedSceneCache mechanism and therefore parallel threads could be querying at same time.
Added a unit test to demonstrate that.

src/IECore/SceneCache.cpp
@@ -519,7 +519,15 @@ class SceneCache::ReaderImplementation : public SceneCache::Implementation
 
 		inline const SampleTimes &attributeSampleTimes( const SceneCache::Name &name ) const
 		{
-			// \todo consider tbb::concurrent_hash_map instead
+			AttributeMapMutex::scoped_lock lock( m_attributeMutex, false );
+			AttributeSamplesMap::const_iterator cit = m_attributeSampleTimes.find( name );
+			if ( cit != m_attributeSampleTimes.end() )
+			{
+				return *(cit->second);
+			}
+
+			lock.upgrade_to_writer();
+
 			std::pair< AttributeSamplesMap::iterator, bool > it = m_attributeSampleTimes.insert( std::pair< IndexedIO::EntryID, SampleTimes* >( name, NULL ) );
 			if ( it.second )
 			{
@@ -725,6 +733,7 @@ class SceneCache::ReaderImplementation : public SceneCache::Implementation
 		// \todo Consider using concurrent_vector for constant access time.
 		typedef tbb::concurrent_hash_map< uint64_t, SampleTimes > SampleTimesMap;
 		typedef std::map< IndexedIO::EntryID, const SampleTimes* > AttributeSamplesMap;
+		typedef tbb::spin_rw_mutex AttributeMapMutex;
 
 		typedef std::pair< const ReaderImplementation *, size_t > SimpleCacheKey;
 		typedef tuple< const ReaderImplementation *, const SceneCache::Name &, size_t > AttributeCacheKey;
@@ -843,6 +852,7 @@ class SceneCache::ReaderImplementation : public SceneCache::Implementation
 		mutable const SampleTimes *m_boundSampleTimes;
 		mutable const SampleTimes *m_transformSampleTimes;
 		mutable AttributeSamplesMap m_attributeSampleTimes;
+		mutable AttributeMapMutex m_attributeMutex;
 		mutable const SampleTimes *m_objectSampleTimes;
 
 		IndexedIOPtr globalSampleTimes() const

test/IECore/SceneCacheThreadingTest.cpp
@@ -1,0 +1,130 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Image Engine Design nor the names of any
+//       other contributors to this software may be used to endorse or
+//       promote products derived from this software without specific prior
+//       written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include <vector>
+#include <iostream>
+#include "tbb/tbb.h"
+#include "IECore/SharedSceneInterfaces.h"
+#include "SceneCacheThreadingTest.h"
+
+using namespace boost;
+using namespace boost::unit_test;
+using namespace tbb;
+
+namespace IECore
+{
+
+struct SceneCacheThreadingTest
+{
+	
+	struct TestSceneCache
+	{
+		public :
+			
+			TestSceneCache() : m_errors( 0 )
+			{
+			}
+
+			void operator() ( int instance ) const
+			{
+				for ( size_t i = 0; i < 1000; i++ )
+				{
+					if ( (instance + i) % 7 == 0 )
+					{
+						SharedSceneInterfaces::clear();
+					}
+					ConstSceneInterfacePtr scene = SharedSceneInterfaces::get("test/IECore/data/sccFiles/attributeAtRoot.scc");
+					try 
+					{
+						scene->readAttribute("w", 0);
+					}
+					catch( Exception e )
+					{
+						m_errors++;
+					}
+				}
+			}
+
+			size_t errors() const
+			{
+				return m_errors;
+			}
+		
+		private :
+			mutable size_t m_errors;
+			SceneInterface::Path m_attributePath;
+	};
+
+	void testAttributeRead()
+	{
+		const int numThreads = 100;
+
+		TestSceneCache task;
+		std::vector< tbb::tbb_thread *> threads;
+
+		for ( int i = 0; i < numThreads; i++ )
+		{
+			threads.push_back( new tbb::tbb_thread( task, i ) );
+		}
+		for ( int i = 0; i < numThreads; i++ )
+		{
+			threads[i]->join();
+		}
+		for ( int i = 0; i < numThreads; i++ )
+		{
+			delete threads[i];
+		}
+		BOOST_CHECK( task.errors() == 0 );
+
+	}
+
+};
+
+struct SceneCacheThreadingTestSuite : public boost::unit_test::test_suite
+{
+
+	SceneCacheThreadingTestSuite() : boost::unit_test::test_suite( "SceneCacheThreadingTestSuite" )
+	{
+		boost::shared_ptr<SceneCacheThreadingTest> instance( new SceneCacheThreadingTest() );
+
+		add( BOOST_CLASS_TEST_CASE( &SceneCacheThreadingTest::testAttributeRead, instance ) );
+	}
+};
+
+void addSceneCacheThreadingTest(boost::unit_test::test_suite* test)
+{
+	test->add( new SceneCacheThreadingTestSuite( ) );
+}
+
+} // namespace IECore

test/IECore/IECoreTest.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007-2013, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2007-2014, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -78,6 +78,7 @@
 #include "CompoundDataTest.h"
 #include "CompoundObjectTest.h"
 #include "ComputationCacheTest.h"
+#include "SceneCacheThreadingTest.h"
 
 using namespace boost::unit_test;
 using boost::test_tools::output_test_stream;
@@ -123,6 +124,7 @@ test_suite* init_unit_test_suite( int argc, char* argv[] )
 		addCompoundDataTest(test);
 		addCompoundObjectTest(test);
 		addComputationCacheTest(test);
+		addSceneCacheThreadingTest(test);
 	}
 	catch (std::exception &ex)
 	{

test/IECore/SceneCacheThreadingTest.h
@@ -1,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014, Image Engine Design Inc. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Image Engine Design nor the names of any
+//       other contributors to this software may be used to endorse or
+//       promote products derived from this software without specific prior
+//       written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef IECORE_SCENECACHETHREADINGTEST_H
+#define IECORE_SCENECACHETHREADINGTEST_H
+
+#include "boost/test/unit_test.hpp"
+
+namespace IECore
+{
+
+void addSceneCacheThreadingTest( boost::unit_test::test_suite *test );
+
+}
+
+#endif // IECORE_SCENECACHETHREADINGTEST_H
+

commit 9f05a15c0355edbd9aefaefd822f58f03a4fc8a1

Fixing seg fault when attempting to read a non-existant attribute several times.
This issue was introduced by 030e7c936de091e22564acec4bbffcb222154286

src/IECore/SceneCache.cpp
@@ -535,6 +535,7 @@ class SceneCache::ReaderImplementation : public SceneCache::Implementation
 			}
 			if ( !it.first->second )
 			{
+				m_attributeSampleTimes.erase( it.first );
 				throw Exception( ( boost::format( "No samples for attribute %s available" ) % name.value() ).str() );
 			}
 			return *(it.first->second);

test/IECore/SceneCacheThreadingTest.cpp
@@ -112,6 +112,16 @@ struct SceneCacheThreadingTest
 		parallel_reduce( blocked_range<size_t>( 0, 100 ), task );
  		BOOST_CHECK( task.errors() == 0 );
 	}
+	
+	void testFakeAttributeRead()
+	{
+		task_scheduler_init scheduler( 100 );
+		
+		TestSceneCache task( "fake" );
+		
+		parallel_reduce( blocked_range<size_t>( 0, 100 ), task );
+ 		BOOST_CHECK( task.errors() == 100000 );
+	}
 
 };
 
@@ -123,6 +133,7 @@ struct SceneCacheThreadingTestSuite : public boost::unit_test::test_suite
 		boost::shared_ptr<SceneCacheThreadingTest> instance( new SceneCacheThreadingTest() );
 
 		add( BOOST_CLASS_TEST_CASE( &SceneCacheThreadingTest::testAttributeRead, instance ) );
+		add( BOOST_CLASS_TEST_CASE( &SceneCacheThreadingTest::testFakeAttributeRead, instance ) );
 	}
 };
 