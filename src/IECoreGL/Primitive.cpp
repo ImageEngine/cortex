ImageEngine/cortex

commit 5ef1dcf77bf48bb8b26c2733b5c7b948e37d1075

Wireframe shading for SceneShapes, use Cs instead of vertexCs.

src/IECoreGL/Primitive.cpp
@@ -130,6 +130,55 @@ static Shader *constant2()
 	return s.get();
 }
 
+
+static Shader *flatConstant()
+{
+
+	static const char *vertexSource =
+
+		"#version 120\n"
+		""
+		"#if __VERSION__ <= 120\n"
+		"#define in attribute\n"
+		"#define out varying\n"
+		"#endif\n"
+		""
+		"uniform vec3 Cs = vec3( 1, 1, 1 );"
+		""
+		"in vec3 vertexP;"
+		"in vec3 vertexCs;"
+		""
+		"out vec3 geometryCs;"
+		""
+		"out vec3 fragmentCs;"
+		""
+		"void main()"
+		"{"
+		"	vec4 pCam = gl_ModelViewMatrix * vec4( vertexP, 1 );"
+		"	gl_Position = gl_ProjectionMatrix * pCam;"
+		"	geometryCs = Cs;"
+		""
+		"	fragmentCs = geometryCs;"
+		"}";
+
+	static const char *fragmentSource =
+
+		"#if __VERSION__ <= 120\n"
+		"#define in varying\n"
+		"#endif\n"
+		""
+		"in vec3 fragmentCs;"
+		""
+		"void main()"
+		"{"
+		"	gl_FragColor = vec4( fragmentCs, 1 );"
+		"}";
+
+	static ShaderPtr s = new Shader( vertexSource, fragmentSource );
+	return s.get();
+}
+
+
 void Primitive::render( State *state ) const
 {
 	const Selector *currentSelector = Selector::currentSelector();
@@ -193,7 +242,7 @@ void Primitive::render( State *state ) const
 	}
 	
 	// get a constant shader suitable for drawing wireframes, points etc.
-	const Shader *constantShader = Shader::constant();
+	const Shader *constantShader = flatConstant();
 	if( currentSelector && currentSelector->mode() == Selector::IDRender )
 	{
 		// if we're in IDRender mode, then the constant shader is unsuitable,
@@ -284,7 +333,7 @@ const Shader::Setup *Primitive::shaderSetup( const Shader *shader, State *state 
 	}
 	
 	Shader::SetupPtr setup = new Shader::Setup( shader );
-	addPrimitiveVariablesToShaderSetup( setup.get()  );
+	addPrimitiveVariablesToShaderSetup( setup.get() );
 		
 	m_shaderSetups.push_back( setup );
 	return setup.get();

commit 3c05d77cf5b0b2891710f663f709942a5d0f0e42

Fixed IECoreGL PointPrimitive test regression.
This fixes the problems introduced by 5ef1dcf77bf48bb8b26c2733b5c7b948e37d1075.

The issue is that IECoreGL::PointsPrimitive overrides shaderSetup() in order to tinker with the vertex shader to perform fast instanced rendering of disks, spheres and quads. Because we do want to allow users to provide their own vertex shaders if they wish, the PointsPrimitive makes the assumption that if a vertex source has been specified explicitly, that the user knows what they're doing and will take care of the instancing too. This interacted badly with the new flatConstant() shader that the Primitive was using to override wireframe shading - because that shader was explicitly specifying a vertex source.

The solution is simple - use the default vertex source in flatConstant(), and grab the uniformCs variable directly in the fragment source. I should have thought of it in the first place.

src/IECoreGL/Primitive.cpp
@@ -130,51 +130,19 @@ static Shader *constant2()
 	return s.get();
 }
 
-
 static Shader *flatConstant()
 {
 
-	static const char *vertexSource =
-
-		"#version 120\n"
-		""
-		"#if __VERSION__ <= 120\n"
-		"#define in attribute\n"
-		"#define out varying\n"
-		"#endif\n"
-		""
-		"uniform vec3 Cs = vec3( 1, 1, 1 );"
-		""
-		"in vec3 vertexP;"
-		"in vec3 vertexCs;"
-		""
-		"out vec3 geometryCs;"
-		""
-		"out vec3 fragmentCs;"
-		""
-		"void main()"
-		"{"
-		"	vec4 pCam = gl_ModelViewMatrix * vec4( vertexP, 1 );"
-		"	gl_Position = gl_ProjectionMatrix * pCam;"
-		"	geometryCs = Cs;"
-		""
-		"	fragmentCs = geometryCs;"
-		"}";
-
 	static const char *fragmentSource =
 
-		"#if __VERSION__ <= 120\n"
-		"#define in varying\n"
-		"#endif\n"
-		""
-		"in vec3 fragmentCs;"
+		"uniform vec3 Cs;" // get colour from uniform Cs, bypassing vertexCs
 		""
 		"void main()"
 		"{"
-		"	gl_FragColor = vec4( fragmentCs, 1 );"
+		"	gl_FragColor = vec4( Cs, 1 );"
 		"}";
 
-	static ShaderPtr s = new Shader( vertexSource, fragmentSource );
+	static ShaderPtr s = new Shader( "", fragmentSource );
 	return s.get();
 }
 