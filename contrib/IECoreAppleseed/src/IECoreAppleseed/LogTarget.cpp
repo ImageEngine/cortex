9b279a5cfef0128c24162ce85a75c91666a80842
est77 <ramenhdr@gmail.com>
2015-03-22 16:17:36 +0100

    Added an appleseed log handler that uses MessageHandler to log messages.

---
+++ contrib/IECoreAppleseed/src/IECoreAppleseed/LogTarget.cpp
@@ -0,0 +1,71 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2015, Esteban Tovagliari. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Image Engine Design nor the names of any
+//       other contributors to this software may be used to endorse or
+//       promote products derived from this software without specific prior
+//       written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "IECoreAppleseed/private/LogTarget.h"
+
+#include "IECore/MessageHandler.h"
+
+using namespace IECore;
+
+namespace asf = foundation;
+
+void IECoreAppleseed::IECoreLogTarget::release()
+{
+	delete this;
+}
+
+void IECoreAppleseed::IECoreLogTarget::write( const asf::LogMessage::Category category, const char* file,
+			const size_t line, const char* header, const char* message)
+{
+	switch( category )
+	{
+		case asf::LogMessage::Debug:
+			msg( MessageHandler::Debug, "appleseed", message );
+		break;
+
+		case asf::LogMessage::Warning:
+			msg( MessageHandler::Warning, "appleseed", message );
+		break;
+
+		case asf::LogMessage::Error:
+		case asf::LogMessage::Fatal:
+			msg( MessageHandler::Error, "appleseed", message );
+		break;
+
+		default:
+		case asf::LogMessage::Info:
+			msg( MessageHandler::Info, "appleseed", message );
+		break;
+	}
+}
