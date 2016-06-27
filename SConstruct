ImageEngine/cortex

commit 845eb9592f0af15a24ff6b96713d576f2544ccbd

Fixing SConstruct configuration for Appleseed

SConstruct
@@ -3278,7 +3278,17 @@ haveAppleseed = False
 
 if doConfigure :
 
-	c = Configure( appleseedEnv )
+	# Since we only build shared libraries and not exectuables,
+	# we only need to check that shared libs will link correctly.
+	# This is necessary for appleseed, which uses
+	# a run-time compatible, but link-time incompatbile libstdc++
+	# in some obscure studio setups. This approach succeeds because
+	# building a shared library doesn't require resolving the
+	# unresolved symbols of the libraries that it links to.
+	appleseedCheckEnv = appleseedEnv.Clone()
+	appleseedCheckEnv.Append( CXXFLAGS = [ "-fPIC" ] )
+	appleseedCheckEnv.Append( LINKFLAGS = [ "-shared" ] )
+	c = Configure( appleseedCheckEnv )
 
 	if not c.CheckLibWithHeader( "appleseed", "renderer/api/rendering.h", "CXX" ) :
 

commit 76f08239ad476e53709d7cb68c0f62733fffec90

SConstruct : Fix appleseed linking on OS X.
This appears to have been broken by 845eb9592f0af15a24ff6b96713d576f2544ccbd.

SConstruct
@@ -3306,9 +3306,15 @@ if doConfigure :
 
 		c.Finish()
 
-		# we can't append this before configuring, as then it gets built as
-		# part of the configure process
-		appleseedEnv.Append( LIBS = os.path.basename( coreEnv.subst( "$INSTALL_LIB_NAME" ) ) )
+		appleseedEnv.Append(
+			LIBS = [
+				"appleseed",
+				# We can't append this before configuring, as then it gets built as
+				# part of the configure process
+				os.path.basename( coreEnv.subst( "$INSTALL_LIB_NAME" ) ),
+			]
+		)
+
 		appleseedPythonModuleEnv.Append( LIBS = os.path.basename( corePythonEnv.subst( "$INSTALL_PYTHONLIB_NAME" ) ) )
 
 		appleseedDriverEnv.Append(