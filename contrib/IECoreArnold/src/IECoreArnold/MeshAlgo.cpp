3a6d317389fbf0280c1bebbae0404721701c9cc6
Andrew Kaufman <andrewk.imageengine@gmail.com>
2017-09-12 17:48:02 -0700

    IECoreArnold : Update for UV prim var and indices.

--- contrib/IECoreArnold/src/IECoreArnold/MeshAlgo.cpp
+++ contrib/IECoreArnold/src/IECoreArnold/MeshAlgo.cpp
@@ -84,121 +84,44 @@ const T *variableData( const PrimitiveVariableMap &variables, const std::string 
 	return runTimeCast<T>( it->second.data.get() );
 }
 
-// Cortex represents UV sets horribly, with the u and v stored as separate primitive variables.
-// RenderMan's FaceVarying data format is also horrible for UV sets, because it throws away all
-// connectivity information, duplicating UVs so that each face vertex has its own value. To work
-// around this, Cortex stores a third variable holding the indices into the original data, so we
-// have a record of the connectivity. This is all done by convention, and because that's what the
-// FromMayaMeshConverter has evolved to do.
-//
-// All this plays poorly with Arnold, because it stores UVs sanely, as an array of 2d vectors with
-// an indices array to index into them per face-vertex. So this function converts the Cortex
-// representation into the Arnold form, so that Arnold can use the connectivity to correctly
-// interpolate the uvs.
-//
-// We should really improve the Cortex representation as follows :
-//
-// - Use V2fVectorData for UVs.
-// - Add an optional IntVectorData indices array into the PrimitiveVariable class to store
-//   indices into the UV array, and associating the indices directly with the data.
-// - Use the above to avoid the duplication of UVs when storing FaceVarying UVs, so we can
-//   do a straightforward conversion to Arnold, and only do the dumb duplication on conversion
-///  to RenderMan.
-// - Perhaps also add a role enum to PrimitiveVariable, so we can distinguish between UVs and
-///  things that just happen to hold V2fVectorData.
-void convertIndexedUVSet( const std::string &setName, PrimitiveVariableMap &variables, AtNode *node )
+void convertUVSet( PrimitiveVariableMap::const_iterator uvIt, const vector<int> &vertexIds, AtNode *node )
 {
-	const std::string sName = setName == "" ? "s" : setName + "_s";
-	const std::string tName = setName == "" ? "t" : setName + "_t";
-	const std::string indicesName = setName == "" ? "stIndices" : setName + "Indices";
+	const V2fVectorData *uvData = runTimeCast<V2fVectorData>( uvIt->second.data.get() );
 
-	const FloatVectorData *sData = variableData<FloatVectorData>( variables, sName, PrimitiveVariable::FaceVarying );
-	const FloatVectorData *tData = variableData<FloatVectorData>( variables, tName, PrimitiveVariable::FaceVarying );
-	const IntVectorData *indicesData = variableData<IntVectorData>( variables, indicesName, PrimitiveVariable::FaceVarying );
-
-	if( !( sData && tData && indicesData ) )
+	if( !uvData )
 	{
 		return;
 	}
 
-	const vector<float> &s = sData->readable();
-	const vector<float> &t = tData->readable();
-	const vector<int> &indices = indicesData->readable();
-
-	int numUVs = 1 + *max_element( indices.begin(), indices.end() );
-	AtArray *uvsArray = AiArrayAllocate( numUVs, 1, AI_TYPE_POINT2 );
-	AtArray *indicesArray = AiArrayAllocate( indices.size(), 1, AI_TYPE_UINT );
-
-	for( size_t i = 0, e = indices.size(); i < e; ++i )
-	{
-		AtPoint2 uv = { s[i], t[i] };
-		AiArraySetPnt2( uvsArray, indices[i], uv );
-		AiArraySetUInt( indicesArray, i, indices[i] );
-	}
-
-	if( setName == "" )
-	{
-		AiNodeSetArray( node, "uvlist", uvsArray );
-		AiNodeSetArray( node, "uvidxs", indicesArray );
-	}
-	else
-	{
-		AiNodeDeclare( node, setName.c_str(), "indexed POINT2" );
-		AiNodeSetArray( node, setName.c_str(), uvsArray );
-		AiNodeSetArray( node, (setName + "idxs").c_str(), indicesArray );
-	}
-
-	variables.erase( sName );
-	variables.erase( tName );
-	variables.erase( indicesName );
-}
-
-// Version of the above for when we have no indices available.
-void convertUVSet( const std::string &setName, PrimitiveVariableMap &variables, const vector<int> &vertexIds, AtNode *node )
-{
-	const std::string sName = setName == "" ? "s" : setName + "_s";
-	const std::string tName = setName == "" ? "t" : setName + "_t";
-
-	const FloatVectorData *sData = variableData<FloatVectorData>( variables, sName );
-	const FloatVectorData *tData = variableData<FloatVectorData>( variables, tName );
-
-	if( !( sData && tData ) )
-	{
-		return;
-	}
-
-	PrimitiveVariable::Interpolation sInterpolation = variables.find( "s" )->second.interpolation;
-	PrimitiveVariable::Interpolation tInterpolation = variables.find( "t" )->second.interpolation;
-	if( sInterpolation != tInterpolation )
+	if( uvIt->second.interpolation != PrimitiveVariable::Varying && uvIt->second.interpolation != PrimitiveVariable::Vertex && uvIt->second.interpolation != PrimitiveVariable::FaceVarying )
 	{
 		msg(
 			Msg::Warning, "ToArnoldMeshConverter::doConversion",
-			boost::format( "Variables \"%s\" and \"%s\" have different interpolation - not generating uvs." ) % sName % tName
+			boost::format( "Variable \"%s\" has an invalid interpolation type - not generating uvs." ) % uvIt->first
 		);
 		return;
 	}
 
-	if( sInterpolation != PrimitiveVariable::Varying && sInterpolation != PrimitiveVariable::Vertex && sInterpolation != PrimitiveVariable::FaceVarying )
-	{
-		msg(
-			Msg::Warning, "ToArnoldMeshConverter::doConversion",
-			boost::format( "Variables \"%s\" and \"%s\" have different interpolation types - not generating uvs." ) % sName % tName
-		);
-		return;
-	}
+	const vector<Imath::V2f> &uvs = uvData->readable();
 
-	const vector<float> &s = sData->readable();
-	const vector<float> &t = tData->readable();
-
-	AtArray *uvsArray = AiArrayAllocate( s.size(), 1, AI_TYPE_POINT2 );
-	for( size_t i = 0, e = s.size(); i < e; ++i )
+	AtArray *uvsArray = AiArrayAllocate( uvs.size(), 1, AI_TYPE_POINT2 );
+	for( size_t i = 0, e = uvs.size(); i < e; ++i )
 	{
-		AtPoint2 uv = { s[i], t[i] };
+		AtPoint2 uv = { uvs[i][0], uvs[i][1] };
 		AiArraySetPnt2( uvsArray, i, uv );
 	}
 
-	AtArray *indicesArray = NULL;
-	if( sInterpolation == PrimitiveVariable::FaceVarying )
+	AtArray *indicesArray = nullptr;
+	if( uvIt->second.indices )
+	{
+		const vector<int> &indices = uvIt->second.indices->readable();
+		indicesArray = AiArrayAllocate( indices.size(), 1, AI_TYPE_UINT );
+		for( size_t i = 0, e = indices.size(); i < e; ++i )
+		{
+			AiArraySetUInt( indicesArray, i, indices[i] );
+		}
+	}
+	else if( uvIt->second.interpolation == PrimitiveVariable::FaceVarying )
 	{
 		indicesArray = identityIndices( vertexIds.size() );
 	}
@@ -211,20 +134,17 @@ void convertUVSet( const std::string &setName, PrimitiveVariableMap &variables, 
 		}
 	}
 
-	if( setName == "" )
+	if( uvIt->first == "uv" )
 	{
 		AiNodeSetArray( node, "uvlist", uvsArray );
 		AiNodeSetArray( node, "uvidxs", indicesArray );
 	}
 	else
 	{
-		AiNodeDeclare( node, setName.c_str(), "indexed POINT2" );
-		AiNodeSetArray( node, setName.c_str(), uvsArray );
-		AiNodeSetArray( node, (setName + "idxs").c_str(), indicesArray );
+		AiNodeDeclare( node, uvIt->first.c_str(), "indexed POINT2" );
+		AiNodeSetArray( node, uvIt->first.c_str(), uvsArray );
+		AiNodeSetArray( node, (uvIt->first + "idxs").c_str(), indicesArray );
 	}
-
-	variables.erase( sName );
-	variables.erase( tName );
 }
 
 AtNode *convertCommon( const IECore::MeshPrimitive *mesh )
@@ -265,37 +185,25 @@ AtNode *convertCommon( const IECore::MeshPrimitive *mesh )
 	variablesToConvert.erase( "P" ); // These will be converted
 	variablesToConvert.erase( "N" ); // outside of this function.
 
-	// Convert and remove indexed UVs first. We must perform
-	// the iteration to find the names separately to the iteration
-	// to convert them, because convertIndexedUVSet() removes items
-	// from variablesToConvert, and would therefore invalidate
-	// the interators we were using if we were to do it in one loop.
-	vector<string> uvSetNames;
+	// Find all UV sets. We must perform the iteration to find the
+	// names separately to the iteration to convert them, because
+	// convertUVSet() removes items from variablesToConvert, and
+	// would therefore invalidate the interators we were using if
+	// we were to do it in one loop.
+	vector<PrimitiveVariableMap::iterator> uvSets;
 	for( PrimitiveVariableMap::iterator it = variablesToConvert.begin(), eIt = variablesToConvert.end(); it != eIt; ++it )
 	{
-		if( boost::ends_with( it->first, "Indices" ) )
+		/// \todo: add a role enum to PrimitiveVariable, so we can distinguish between UVs and
+		///  things that just happen to hold V2fVectorData.
+		if( it->second.data->typeId() == V2fVectorDataTypeId )
 		{
-			uvSetNames.push_back( it->first == "stIndices" ? "" : it->first.substr( 0, it->first.size() - 7 ) );
+			uvSets.push_back( it );
 		}
 	}
-	for( vector<string>::const_iterator it = uvSetNames.begin(), eIt = uvSetNames.end(); it != eIt; ++it )
+	for( auto &it : uvSets )
 	{
-		convertIndexedUVSet( *it, variablesToConvert, result );
-	}
-
-	// Then convert and remove non-indexed uvs. As above, we must
-	// do this in two phases.
-	uvSetNames.clear();
-	for( PrimitiveVariableMap::iterator it = variablesToConvert.begin(), eIt = variablesToConvert.end(); it != eIt; ++it )
-	{
-		if( it->first == "s" || boost::ends_with( it->first, "_s" ) )
-		{
-			uvSetNames.push_back( it->first == "s" ? "" : it->first.substr( 0, it->first.size() - 2 ) );
-		}
-	}
-	for( vector<string>::const_iterator it = uvSetNames.begin(), eIt = uvSetNames.end(); it != eIt; ++it )
-	{
-		convertUVSet( *it, variablesToConvert, vertexIds, result );
+		::convertUVSet( it, vertexIds, result );
+		variablesToConvert.erase( it );
 	}
 
 	// Finally, do a generic conversion of anything that remains.
