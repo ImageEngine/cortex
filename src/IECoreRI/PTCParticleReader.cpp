db2d16aca973134c89459e526ec2450b013c373f
John Haddon <thehaddonyoof@gmail.com>
2017-11-16 16:31:46 +0000

    DespatchTypedData : Remove TypedDataInterpolation
    This would represent a dependency between IECore and IECoreScene, so we must break it somehow. Since it is only used in PTCParticleReader, and a simpler implementation is now possible anyway, it's easier to just remove it and add an equivalent inline in PTCParticleReader.

--- src/IECoreRI/PTCParticleReader.cpp
+++ src/IECoreRI/PTCParticleReader.cpp
@@ -278,7 +278,15 @@ ObjectPtr PTCParticleReader::doOperation( const CompoundObject * operands )
 
 		DataPtr d = itData->second;
 
-		PrimitiveVariable::Interpolation interp = despatchTypedData< TypedDataInterpolation, TypeTraits::IsTypedData, DespatchTypedDataIgnoreError >( const_cast<Data *>( d.get() ) );
+		PrimitiveVariable::Interpolation interp = PrimitiveVariable::Invalid;
+		if( despatchTraitsTest<TypeTraits::IsSimpleTypedData>( d.get() ) )
+		{
+			interp = PrimitiveVariable::Constant;
+		}
+		else if( despatchTraitsTest<TypeTraits::IsVectorTypedData>( d.get() ) )
+		{
+			interp = PrimitiveVariable::Vertex;
+		}
 
 		if ( interp == PrimitiveVariable::Invalid )
 		{
