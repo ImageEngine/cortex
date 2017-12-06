5db965793fb192f31a77c51278d5198316713f6c
Carmen Pinto <carmen.n.pinto@gmail.com>
2017-12-05 17:06:48 -0800

    switch to ducktyping instead of comparison

--- python/IECoreHoudini/FnParameterisedHolder.py
+++ python/IECoreHoudini/FnParameterisedHolder.py
@@ -88,9 +88,9 @@ class FnParameterisedHolder():
 		if contextArgs.get( "shiftclick", False ) :
 			converter = holder.parent().createNode( "ieCortexConverter", node_name = holder.name()+"Converter" )
 			outputNode = hou.node( contextArgs.get( "outputnodename", "" ) )
-			if hou.applicationVersion()[0] >= 16 and hou.applicationVersion()[1] >= 5:
+			try:
 				toolutils.connectMultiInputsAndOutputs( converter, False, [( holder, 0, 0 )] if holder else None, [( outputNode, 0, 0 )] if outputNode else None )
-			else:
+			except AttributeError:
 				toolutils.connectInputsAndOutputs( converter, False, holder, outputNode, 0, 0 )
 			x, y = holder.position()
 			converter.setPosition( [x,y-1] )
