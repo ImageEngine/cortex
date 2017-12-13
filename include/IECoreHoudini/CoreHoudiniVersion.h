8e524086dbd2a1f706e96f6aa5d62f8bfd68bd21
Carmen Pinto <carmen.n.pinto@gmail.com>
2017-12-13 15:58:10 -0800

    copyright year change

--- include/IECoreHoudini/CoreHoudiniVersion.h
+++ include/IECoreHoudini/CoreHoudiniVersion.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2010-2018, Image Engine Design Inc. All rights reserved.
+//  Copyright (c) 2010-2017, Image Engine Design Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
