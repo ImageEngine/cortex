aa7dd5224098c3a90b52d6168044478d62c7cca8
John Haddon <thehaddonyoof@gmail.com>
2018-01-31 14:18:36 +0000

    Appleseed : Refactor remaining tests into MeshAlgoTest/TransformAlgoTest

--- contrib/IECoreAppleseed/test/IECoreAppleseed/All.py
+++ contrib/IECoreAppleseed/test/IECoreAppleseed/All.py
@@ -1,6 +1,7 @@
 ##########################################################################
 #
 #  Copyright (c) 2014, Esteban Tovagliari. All rights reserved.
+#  Copyright (c) 2018, Image Engine Design Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
@@ -37,8 +38,8 @@ import unittest
 
 import IECore
 
-from MeshTest import MeshTest
-from MotionTest import MotionTest
+from MeshAlgoTest import MeshAlgoTest
+from TransformAlgoTest import TransformAlgoTest
 
 unittest.TestProgram(
 	testRunner = unittest.TextTestRunner(
