ImageEngine/cortex

commit 0c199d5543041b7be0ff24a052ce2e753c02d10c

Canceller binding : Derive Cancelled class from RunTimeError
It turns out that Python has problems with exceptions that don't derive from the python `Exception` class. The symptom was a `"SystemError: 'finally' pops bad exception"` error in `testFinally()`. There's a bit more information in [this thread](https://mail.python.org/pipermail/cplusplus-sig/2012-June/016649.html).

src/IECorePython/CancellerBinding.cpp
@@ -48,7 +48,6 @@ namespace IECorePython
 
 void bindCanceller()
 {
-	class_<Cancelled>( "Cancelled");
 
 	class_<Canceller, boost::noncopyable>( "Canceller" )
 		.def( "cancel", &Canceller::cancel )
@@ -58,13 +57,17 @@ void bindCanceller()
 
 	register_ptr_to_python<std::shared_ptr<Canceller>>();
 
+	PyObject *cancelledClass = PyErr_NewException( (char *)"IECore.Cancelled", PyExc_RuntimeError, nullptr );
+
 	register_exception_translator<Cancelled>(
-		[]( const Cancelled &e ) {
-			object o( e );
-			Py_INCREF( o.ptr() );
-			PyErr_SetObject( (PyObject *)o.ptr()->ob_type, o.ptr() );
+		[cancelledClass]( const Cancelled &e ) {
+			PyObject *value = PyObject_CallFunction( cancelledClass, nullptr );
+			PyErr_SetObject( cancelledClass, value );
 		}
 	);
+
+	scope().attr( "Cancelled" ) = object( borrowed( cancelledClass ) );
+
 }
 
 } // namespace IECorePython

test/IECore/CancellerTest.py
@@ -51,5 +51,27 @@ class CancellerTest( unittest.TestCase ) :
 		with self.assertRaises( IECore.Cancelled ) :
 			IECore.Canceller.check( c )
 
+	def testFinally( self ) :
+
+		def f() :
+
+			c = IECore.Canceller()
+			c.cancel()
+
+			didFinally = False
+			try :
+				IECore.Canceller.check( c )
+			finally :
+				didFinally = True
+
+			self.assertEqual( didFinally, True )
+
+		self.assertRaises( IECore.Cancelled, f )
+
+	def testCancelledClass( self ) :
+
+		self.assertIsInstance( IECore.Cancelled(), RuntimeError )
+		self.assertEqual( IECore.Cancelled.__module__, "IECore" )
+
 if __name__ == "__main__":
 	unittest.main()

commit 0ef6a4c450c6cf06ad9ba8d056b8661b5f8fc74e

Canceller binding : Fix Python->C++ conversion for `IECore.Cancelled`
This was broken by 0c199d5543041b7be0ff24a052ce2e753c02d10c. `ExceptionAlgo::translatePythonException()` relies on `extract<Cancelled>()` to determine if the exception is an instance of `Cancelled`. When we used `boost::class` to bind Cancelled this worked automatically, but now we bind manually via `PyErr_NewException()` we are responsible for providing a Python->C++ type conversion ourselves.

Back in #773 when I introduced the Canceller, I was aware of the importance of this translation, but didn't include a test case, claiming it wasn't possible to test from a pure Python unit test. I consoled myself that I had tests in Gaffer that would catch the problem instead. That's true, but today they only caught the problem after a lengthy update cycle of GafferHQ/dependencies, leaving me back at square one. The lesson : put your tests as close to the thing they are testing as possible. This time I've managed to find a way of testing it, using the exception translation in the LRUCache bindings.

src/IECorePython/CancellerBinding.cpp
@@ -43,6 +43,41 @@
 using namespace boost::python;
 using namespace IECore;
 
+namespace
+{
+
+PyObject *g_cancelledClass = nullptr;
+
+struct CancelledFromPython
+{
+
+	static void registerConverter()
+	{
+		boost::python::converter::registry::push_back(
+			&convertible,
+			&construct,
+			boost::python::type_id<Cancelled>()
+		);
+	}
+
+	private :
+
+		static void *convertible( PyObject *obj )
+		{
+			return PyObject_IsInstance( obj, g_cancelledClass ) ? obj : nullptr;
+		}
+
+		static void construct( PyObject *obj, boost::python::converter::rvalue_from_python_stage1_data *data )
+		{
+			void *storage = ( (converter::rvalue_from_python_storage<Cancelled>*) data )->storage.bytes;
+			new( storage ) Cancelled();
+			data->convertible = storage;
+		}
+
+};
+
+} // namespace
+
 namespace IECorePython
 {
 
@@ -57,16 +92,18 @@ void bindCanceller()
 
 	register_ptr_to_python<std::shared_ptr<Canceller>>();
 
-	PyObject *cancelledClass = PyErr_NewException( (char *)"IECore.Cancelled", PyExc_RuntimeError, nullptr );
+	g_cancelledClass = PyErr_NewException( (char *)"IECore.Cancelled", PyExc_RuntimeError, nullptr );
 
 	register_exception_translator<Cancelled>(
-		[cancelledClass]( const Cancelled &e ) {
-			PyObject *value = PyObject_CallFunction( cancelledClass, nullptr );
-			PyErr_SetObject( cancelledClass, value );
+		[]( const Cancelled &e ) {
+			PyObject *value = PyObject_CallFunction( g_cancelledClass, nullptr );
+			PyErr_SetObject( g_cancelledClass, value );
 		}
 	);
 
-	scope().attr( "Cancelled" ) = object( borrowed( cancelledClass ) );
+	scope().attr( "Cancelled" ) = object( borrowed( g_cancelledClass ) );
+
+	CancelledFromPython::registerConverter();
 
 }
 

test/IECore/CancellerTest.py
@@ -73,5 +73,32 @@ class CancellerTest( unittest.TestCase ) :
 		self.assertIsInstance( IECore.Cancelled(), RuntimeError )
 		self.assertEqual( IECore.Cancelled.__module__, "IECore" )
 
+	def testExceptionTranslation( self ) :
+
+		def raiser( key ) :
+
+			raise IECore.Cancelled()
+
+		c = IECore.LRUCache( raiser, 1 )
+
+		with self.assertRaises( IECore.Cancelled ) as a :
+
+			# Expected sequence of events is as follows :
+			#
+			# 1. `get()` enters C++.
+			# 2. C++ enters back into Python, calling `raiser()`.
+			# 3. `raiser()` raises a _python_ exception containing a _python_
+			#    `IECore.Cancelled` object.
+			# 4. `LRUCacheGetter` (in LRUCacheBinding.cpp) calls `translatePythonException()` to turn the
+			#    Python exception back into an appropriate C++ one - a true instance of `IECore::Cancelled`.
+			# 5. The C++ exception propagates back to `get()`, which translates it back into Python,
+			#    yielding us an instance of `IECore.Cancelled`.
+			#
+			# If 4 or 5 fail to perform the appropriate translation, we'll end up with a generic
+			# exception being thrown, meaning we can no longer determine that cancellation occurred.
+			# And then bad things happen.
+
+			c.get( "test" )
+
 if __name__ == "__main__":
 	unittest.main()