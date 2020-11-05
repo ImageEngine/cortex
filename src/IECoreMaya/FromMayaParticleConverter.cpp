c9fc843ef0a10b19f4c7a26a00f964177affbc3a
Tim Lehr <timle@image-engine.com>
2020-11-04 18:49:03 -0800

    FromMayaParticleConverter: added primvar name remapping capability
    Added the ability to remap a given attribute e.g. `lifespanPP` to
    a more pipeline conform attribute like `age` with the following syntax:
    `lifespanPP=age`. This is only necessary when a remapping is desired.

--- src/IECoreMaya/FromMayaParticleConverter.cpp
+++ src/IECoreMaya/FromMayaParticleConverter.cpp
@@ -45,7 +45,10 @@
 #include "IECore/MessageHandler.h"
 #include "IECoreScene/PointsPrimitive.h"
 
+#include "IECore/AngleConversion.h"
+
 #include "maya/MFnParticleSystem.h"
+#include "maya/MFnDependencyNode.h"
 #include "maya/MPlug.h"
 #if MAYA_API_VERSION >= 201800
 #include "maya/MDGContextGuard.h"
@@ -82,7 +85,12 @@ void FromMayaParticleConverter::constructCommon()
 	IECore::StringVectorDataPtr attributeNamesDefaultData = new IECore::StringVectorData();
 	IECore::StringVectorData::ValueType &attributeNames = attributeNamesDefaultData->writable();
 
+	// add all Maya default per-particle attributes
+	attributeNames.push_back( "position=P" );
 	attributeNames.push_back( "velocity" );
+	attributeNames.push_back( "age" );
+	attributeNames.push_back( "mass" );
+	attributeNames.push_back( "acceleration" );
 
 	m_attributeNamesParameter = new IECore::StringVectorParameter(
 		"attributeNames",
@@ -110,7 +118,7 @@ IECoreScene::PrimitivePtr FromMayaParticleConverter::doPrimitiveConversion( cons
 	MFnParticleSystem fnParticle( object );
 	if( !fnParticle.hasObj( object ) )
 	{
-		throw IECore::InvalidArgumentException( "FromMayaParticleConverter::doPrimitiveConversion : not a particle shape." );
+		throw IECore::InvalidArgumentException( boost::str( boost::format( "FromMayaParticleConverter::doPrimitiveConversion : '%s' is not a particle shape.") % MFnDependencyNode( object ).name().asChar() ) );
 	}
 	return doPrimitiveConversion( fnParticle );
 }
@@ -120,7 +128,7 @@ IECoreScene::PrimitivePtr FromMayaParticleConverter::doPrimitiveConversion( cons
 	MFnParticleSystem fnParticle( dagPath );
 	if( !fnParticle.hasObj( dagPath.node() ) )
 	{
-		throw IECore::InvalidArgumentException( "FromMayaParticleConverter::doPrimitiveConversion : not a particle shape." );
+		throw IECore::InvalidArgumentException( boost::str( boost::format( "FromMayaParticleConverter::doPrimitiveConversion : '%s' is not a particle shape.") % dagPath.partialPathName().asChar() ) );
 	}
 	return doPrimitiveConversion( fnParticle );
 }
@@ -129,16 +137,6 @@ IECoreScene::PrimitivePtr FromMayaParticleConverter::doPrimitiveConversion( MFnP
 {
 	MStatus s;
 	IECoreScene::PointsPrimitivePtr points = new IECoreScene::PointsPrimitive( fnParticle.count( ) );
-
-	/// We always add "position" as "P"
-	MVectorArray position;
-	fnParticle.position( position );
-
-	IECore::V3fVectorDataPtr pos = new IECore::V3fVectorData();
-	pos->setInterpretation( IECore::GeometricData::Point );
-	pos->writable().resize( position.length() );
-	std::transform( MArrayIter<MVectorArray>::begin( position ), MArrayIter<MVectorArray>::end( position ), pos->writable().begin(), IECore::VecConvert<MVector, V3f>() );
-	points->variables["P"] = IECoreScene::PrimitiveVariable( IECoreScene::PrimitiveVariable::Vertex, pos );
 
 	const IECore::StringVectorParameter::ValueType &attributeNames = attributeNamesParameter()->getTypedValue();
 
@@ -175,15 +173,30 @@ IECoreScene::PrimitivePtr FromMayaParticleConverter::doPrimitiveConversion( MFnP
 			IECore::msg(
 				IECore::Msg::Warning,
 				"FromMayaParticleConverter::doPrimitiveConversion",
-				boost::format( "Attribute 'ieParticleAttributes' must be string " )
+				boost::format( "Attribute 'ieParticleAttributes' must be string" )
 			);
 		}
 	}
 
+	bool usesConstantColor = false;
+	if ( fnParticle.hasAttribute( "colorInput" ) )
+	{
+		usesConstantColor = true ? fnParticle.findPlug( "colorInput" ).asInt() == 0: false;
+	}
+
 	for ( IECore::StringVectorParameter::ValueType::const_iterator it = allAttributeNames.begin(); it != allAttributeNames.end(); ++it )
 	{
-		const MString attrName = it->c_str();
-		const std::string &primVarName = *it;
+		MString attrName = it->c_str();
+		std::string primVarName = it->c_str();
+
+		// check if primvar name should be remapped
+		std::vector<std::string> primVarMapping;
+		boost::split( primVarMapping, primVarName, boost::is_any_of( "=" ) );
+		if ( primVarMapping.size() == 2 )
+		{
+			attrName = primVarMapping[ 0 ].c_str();
+			primVarName = primVarMapping[ 1 ].c_str();
+		}
 
 		if ( fnParticle.isPerParticleIntAttribute( attrName, &s ) )
 		{
@@ -206,8 +219,38 @@ IECoreScene::PrimitivePtr FromMayaParticleConverter::doPrimitiveConversion( MFnP
 
 			IECore::FloatVectorDataPtr data = new IECore::FloatVectorData();
 			data->writable().resize( len );
-			data->writable().assign( MArrayIter<MDoubleArray>::begin( arr ), MArrayIter<MDoubleArray>::end( arr ) );
+			if ( attrName == MString("radiusPP") && primVarName == "width" )
+			{
+				// convert radius to width (as used in Cortex / Gaffer)
+				std::vector<float> &dataVector = data->writable();
+				for ( unsigned int i = 0; i < len; i++ )
+				{
+					dataVector[i] = arr[i] * 2.0f;
+				}
+			}
+			else
+			{
+				data->writable().assign( MArrayIter<MDoubleArray>::begin( arr ), MArrayIter<MDoubleArray>::end( arr ) );
+			}
+
 			points->variables[ primVarName ] = IECoreScene::PrimitiveVariable( IECoreScene::PrimitiveVariable::Vertex, data );
+		}
+		else if ( attrName == MString( "rgbPP" ) && ( !fnParticle.isPerParticleVectorAttribute( attrName, &s ) || usesConstantColor ) )
+		{
+			// if there is no RGB per-particle attribute or constant color is used, write a constant primvar instead
+			if ( fnParticle.hasAttribute( "colorRed" ) && fnParticle.hasAttribute( "colorGreen" ) && fnParticle.hasAttribute( "colorBlue" ) )
+			{
+				float r = (float) fnParticle.findPlug( "colorRed" ).asDouble();
+				float g = (float) fnParticle.findPlug( "colorGreen" ).asDouble();
+				float b = (float) fnParticle.findPlug( "colorBlue" ).asDouble();
+
+				IECore::Color3fDataPtr data = new IECore::Color3fData( { r, g, b } );
+				points->variables[ primVarName ] = IECoreScene::PrimitiveVariable( IECoreScene::PrimitiveVariable::Constant, data );
+			}
+			else
+			{
+				IECore::msg( IECore::Msg::Warning, "FromMayaParticleConverter", boost::format( "Node \"%s\", does not have per-particle or per-object color attributes." ) % fnParticle.name().asChar() );
+			}
 		}
 		else if ( fnParticle.isPerParticleVectorAttribute( attrName, &s ) )
 		{
@@ -216,15 +259,58 @@ IECoreScene::PrimitivePtr FromMayaParticleConverter::doPrimitiveConversion( MFnP
 			unsigned int len = fnParticle.getPerParticleAttribute( attrName, arr, &s );
 			assert( len == fnParticle.count() );
 
-			IECore::V3fVectorDataPtr data = new IECore::V3fVectorData();
-			data->setInterpretation( IECore::GeometricData::Vector );
-			data->writable().resize( len );
-		        std::transform( MArrayIter<MVectorArray>::begin( arr ), MArrayIter<MVectorArray>::end( arr ), data->writable().begin(), IECore::VecConvert<MVector, V3f>() );
-			points->variables[ primVarName ] = IECoreScene::PrimitiveVariable( IECoreScene::PrimitiveVariable::Vertex, data );
+			if ( attrName == MString( "rotationPP" ) && primVarName == "orientation" )
+			{
+				// get euler data and convert to quaternion
+				IECore::V3fVectorDataPtr eulerData = new IECore::V3fVectorData();
+				eulerData->writable().resize( len );
+				std::transform( MArrayIter<MVectorArray>::begin( arr ), MArrayIter<MVectorArray>::end( arr ), eulerData->writable().begin(), IECore::VecConvert<MVector, V3f>() );
+
+				auto &readableEulerData = eulerData->readable();
+
+				IECore::QuatfVectorDataPtr quatData = new IECore::QuatfVectorData();
+				auto &writableQuatData = quatData->writable();
+
+				writableQuatData.reserve( readableEulerData.size() );
+
+				for( const auto rotation : readableEulerData )
+				{
+					float x = IECore::degreesToRadians( rotation.x );
+					float y = IECore::degreesToRadians( rotation.y );
+					float z = IECore::degreesToRadians( rotation.z );
+
+					Imath::Eulerf euler( x, y, z, Imath::Eulerf::Default );
+					writableQuatData.push_back( euler.toQuat() );
+				}
+
+				points->variables[ primVarName ] = IECoreScene::PrimitiveVariable( IECoreScene::PrimitiveVariable::Vertex, quatData );
+			}
+			else
+			{
+				IECore::V3fVectorDataPtr data = new IECore::V3fVectorData();
+				if ( primVarName == "P" )
+				{
+					data->setInterpretation( IECore::GeometricData::Point );
+				}
+				else if ( attrName == MString( "rgbPP" ) )
+				{
+					data->setInterpretation( IECore::GeometricData::Color );
+				}
+				else
+				{
+					data->setInterpretation( IECore::GeometricData::Vector );
+				}
+
+				data->writable().resize( len );
+				std::transform( MArrayIter<MVectorArray>::begin( arr ), MArrayIter<MVectorArray>::end( arr ), data->writable().begin(), IECore::VecConvert<MVector, V3f>() );
+				points->variables[ primVarName ] = IECoreScene::PrimitiveVariable( IECoreScene::PrimitiveVariable::Vertex, data );
+			}
+
+
 		}
-		else
+		else if ( attrName.length() > 1 )
 		{
-			IECore::msg( IECore::Msg::Warning, "FromMayaParticleConverter", boost::format( "Ignoring attribute \%s\", which is not a per-particle attribute" ) % primVarName);
+			IECore::msg( IECore::Msg::Warning, "FromMayaParticleConverter", boost::format( "Ignoring attribute \"%s\", which is not a valid per-particle attribute" ) % attrName.asChar() );
 		}
 
 	}
