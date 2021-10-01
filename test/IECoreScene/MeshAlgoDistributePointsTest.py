823715102c4adf386c1efc3448e9455be024c7fc
Eric Mehl <eric@thinkhypothetical.com>
2021-10-01 13:51:18 -0400

    MeshAlgoDistributePointsTest : add additional room for error based on the number of points distributed
    - Different floating point behavior on Windows can result in the nearest primitive to a point being incorrect.
    - See #983 for more discussion.

--- test/IECoreScene/MeshAlgoDistributePointsTest.py
+++ test/IECoreScene/MeshAlgoDistributePointsTest.py
@@ -69,7 +69,7 @@ class MeshAlgoDistributePointsTest( unittest.TestCase ) :
 		for f in range( 0, mesh.verticesPerFace.size() ) :
 			if "density" in mesh :
 				density = mesh["density"].data[f] * origDensity
-			self.assertTrue( abs(pointsPerFace[f] - density * mesh['faceArea'].data[f]) <= density * error )
+			self.assertTrue( abs( pointsPerFace[f] - density * mesh['faceArea'].data[f] ) <= density * error + len(positions) * error * error )
 
 	def testSimple( self ) :
 
