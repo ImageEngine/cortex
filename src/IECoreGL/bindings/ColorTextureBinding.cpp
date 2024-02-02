6872587516962e3ae177ab8d3f4e270c62b1a0bb
Eric Mehl <eric@thinkhypothetical.com>
2024-02-02 13:54:26 -0500

    IECoreGL::Selector : Camera-space depth sampling
    Previously, we were using OpenGL's depth buffer to sample depths. OpenGL
    stores depth samples in a quasi-NDC space range [0.0, 1.0].
    This must then be converted to camera-space using a series of
    multiplication and additions, which accumulate floating point error.
    The end result can have significant error, especially when clipping
    planes are set far apart.
    
    This solution uses `geometryP` output from vertex shaders to get the
    camera-space Z value without precision-reducing floating point
    operations.

--- src/IECoreGL/bindings/ColorTextureBinding.cpp
+++ src/IECoreGL/bindings/ColorTextureBinding.cpp
@@ -50,6 +50,7 @@ void bindColorTexture()
 {
 	IECorePython::RunTimeTypedClass<ColorTexture>()
 		.def( init<unsigned int, unsigned int>() )
+		.def( init<unsigned int, unsigned int, const GLint>() )
 		.def( init<unsigned int, unsigned int, const IECore::Data *, const IECore::Data *, const IECore::Data *, const IECore::Data *>() )
 		.def( init<const IECoreImage::ImagePrimitive *>() )
 	;
