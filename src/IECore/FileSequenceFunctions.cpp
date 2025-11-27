8dc8cc3c4999413a37c5bebbe28a80db8faefb00
Murray Stevenson <50844517+murraystevenson@users.noreply.github.com>
2025-11-27 12:10:41 -0800

    FileSequenceFunctions : Add include required by Boost 1.85

--- src/IECore/FileSequenceFunctions.cpp
+++ src/IECore/FileSequenceFunctions.cpp
@@ -43,6 +43,7 @@
 
 #include "boost/algorithm/string.hpp"
 #include "boost/filesystem/convenience.hpp"
+#include "boost/filesystem/directory.hpp"
 #include "boost/filesystem/operations.hpp"
 #include "boost/filesystem/path.hpp"
 #include "boost/format.hpp"
